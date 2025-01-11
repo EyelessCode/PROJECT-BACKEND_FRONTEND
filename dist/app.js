858b75a3c218a8d3cd7bd22985a7bf366804fce8
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-11 13:39:53 -0500

    Transpilación hecha.
    Transpilación hecha.

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require("dotenv/config");
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 4001;
+app.use(express_1.default.json());
+app.use(express_1.default.static('public/view'));
+app.get('/test', (req, res) => {
+    res.send('PROBANDO SI EL SERVIDOR SI CONECTA!');
+});
+app.listen(PORT, () => console.log(`SERVIDOR ESCUCHANDO EN EL PUERTO ${PORT}`));
+//# sourceMappingURL=app.js.map
