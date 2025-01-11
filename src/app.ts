da6a2e03675ccfbee99b9b2bbdc905bcef992ee9
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-11 13:33:48 -0500

    Update app.ts

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import 'dotenv/config'
+
+const app=express()
+const PORT=process.env.PORT||4001
+
+app.use(express.json())
+app.use(express.static('public/view'))
+
+app.listen(PORT,()=>console.log(`SERVIDOR ESCUCHANDO EN EL PUERTO ${PORT}`))
