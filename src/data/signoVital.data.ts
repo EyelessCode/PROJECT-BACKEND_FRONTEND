ffc76224f05ab437ca823699ee6552d376ad79c9
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-11 23:11:11 -0500

    Update signoVital.data.ts

---
+++ src/data/signoVital.data.ts
@@ -0,0 +1,29 @@
+import { ISignoVital } from "../interface/signoVital.interface";
+
+const SV:ISignoVital[]=[
+    {
+    codigo:1,
+    descripcion:"Presión arterial"
+    },
+    {
+        codigo:2,
+        descripcion:"Frecuencia cardíaca"
+    },
+    {
+        codigo:3,
+        descripcion:"Frecuencia respiratoria"
+    },{
+        codigo:4,
+        descripcion:"Temperatura"
+    },
+    {
+        codigo:5,
+        descripcion:"Peso"
+    },
+    {
+        codigo:6,
+        descripcion:"Dolor de cabeza"
+    }
+]
+
+export {SV}
