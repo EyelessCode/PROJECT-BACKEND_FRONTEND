c12b99a1ec97680aaaffccf5112d7c3330d43fbd
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-12 00:01:49 -0500

    Transpilación de los archivos TS.

---
+++ dist/app.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const signoVital_repositorio_1 = require("./repository/signoVital.repositorio");
+const repositorio = new signoVital_repositorio_1.SignoVital;
+repositorio.obtenerSignos().forEach((signos) => {
+    console.log(`-`.repeat(8) + `\nCódigo: ${signos.codigo} - Descripción: ${signos.descripcion}\n`);
+});
+//# sourceMappingURL=app.test.js.map
---
+++ dist/data/signoVital.data.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SV = void 0;
+const SV = [
+    {
+        codigo: 1,
+        descripcion: "Presión arterial"
+    },
+    {
+        codigo: 2,
+        descripcion: "Frecuencia cardíaca"
+    },
+    {
+        codigo: 3,
+        descripcion: "Frecuencia respiratoria"
+    }, {
+        codigo: 4,
+        descripcion: "Temperatura"
+    },
+    {
+        codigo: 5,
+        descripcion: "Peso"
+    },
+    {
+        codigo: 6,
+        descripcion: "Dolor de cabeza"
+    }
+];
+exports.SV = SV;
+//# sourceMappingURL=signoVital.data.js.map
