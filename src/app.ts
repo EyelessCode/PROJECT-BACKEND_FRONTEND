cfcdce3b4056b02192f35c0712e091525e562d14
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-14 09:36:44 -0500

    Update app.ts
    Tratando de usar la ruta.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express'
 import 'dotenv/config'
+import indexRutas from './routes'
 
 const app=express()
 const PORT=process.env.PORT||4001
 
 app.use(express.json())
-app.use(express.static('public/view'))
+// app.use(express.static('public/view'))
+
+app.use('/',indexRutas)
+
+// Para prueba
 app.get('/test',(req:Request,res:Response)=>{
     res.send('PROBANDO SI EL SERVIDOR SI CONECTA!')
 })
