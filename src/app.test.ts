715223153d967eabce260040ff7491f414158649
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-14 09:39:11 -0500

    Update app.test.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,5 @@
 import { SVEjemplo } from "./data/signoVital.data";
-import { SignoVital } from "./repository/signoVital.repositorio";
+import { SignoVital } from "./repository/signoVital.repository";
 
 const repositorio=new SignoVital
 
