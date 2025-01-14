975a749b055703c575e40790fe148fc6efdbf2b1
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-14 14:24:20 -0500

    Update signoVital.ts

--- src/routes/signoVital.ts
+++ src/routes/signoVital.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { SignoVitalController } from "../controller/signoVital.controller";
 
 const ruta=Router()
+const controlador=new SignoVitalController()
 
-ruta.get('')
+ruta.get('/',controlador.controladorObtenerSignos)
+ruta.get('/:codigo',controlador.controladorObtenerUnSigno)
+ruta.post('/',controlador.controladorCrearSigno)
+ruta.put('/:codigo',controlador.controladorActualizarSigno)
+ruta.delete('/:codigo',controlador.controladorEliminarSigno)
