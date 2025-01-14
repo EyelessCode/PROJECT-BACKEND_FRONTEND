434ce337a3120ad68c980aa24152ac4e734f0924
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-14 09:37:01 -0500

    Update index.ts

---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { readdirSync } from "fs";
+
+const rutas=Router()
+const PATH=`${__dirname}`
+
+const cortarArchivos=(archivo:string)=>{
+    const archivoCortar=archivo.split('.').shift()
+    return archivoCortar
+}
+
+readdirSync(PATH).filter((archivo)=>{
+    const limpiar=cortarArchivos(archivo)
+    
+    if (limpiar!=="index") {
+        import (`/${limpiar}`).then((rutaModulo)=>{
+            console.log(`${limpiar} encontrado/a`);
+            rutas.use(`${limpiar}`,rutaModulo.router)
+        })
+    }
+})
+
+export default rutas;
