09658624fb58d7714535ce37f48cb04ed62eec24
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-14 14:32:45 -0500

    transpilación

--- dist/routes/signoVital.js
+++ dist/routes/signoVital.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ruta = void 0;
 const express_1 = require("express");
 const signoVital_controller_1 = require("../controller/signoVital.controller");
 const ruta = (0, express_1.Router)();
+exports.ruta = ruta;
 const controlador = new signoVital_controller_1.SignoVitalController();
 ruta.get('/', controlador.controladorObtenerSignos);
 ruta.get('/:codigo', controlador.controladorObtenerUnSigno);
