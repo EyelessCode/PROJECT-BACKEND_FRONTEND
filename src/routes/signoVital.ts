0836084d5a8e8b3be27bd6b387b924e0ae7e655f
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-16 18:43:40 -0500

    Update signoVital.ts

--- src/routes/signoVital.ts
+++ src/routes/signoVital.ts
@@ -1,13 +1,30 @@
 import { Router } from "express";
 import { SignoVitalController } from "../infrastructure/controller/signoVital.controller";
+import { prisma } from "../infrastructure/data/prisma.service";
 
 const ruta=Router()
-const controlador=new SignoVitalController()
+/* const controlador=new SignoVitalController()
 
 ruta.get('/',controlador.controladorObtenerSignos)
 ruta.get('/:codigo',controlador.controladorObtenerUnSigno)
 ruta.post('/',controlador.controladorCrearSigno)
 ruta.put('/:codigo',controlador.controladorActualizarSigno)
-ruta.delete('/:codigo',controlador.controladorEliminarSigno)
+ruta.delete('/:codigo',controlador.controladorEliminarSigno) */
+
+ruta.get('/test',async(req,res)=>{
+    try {
+        const signoVitales=prisma.signoVital.findMany()
+        res.json({
+            sucess:true,
+            data:signoVitales
+        })
+    } catch (error) {
+        console.error(`Error al conectar con la BD, ${error}`);
+        res.status(500).json({
+            succes:false,
+            error:"Error al conectar con la BD"
+        })
+    }
+})
 
 export {ruta}
