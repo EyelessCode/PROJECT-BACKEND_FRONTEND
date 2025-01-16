a4f0fa24d5483df2239af8aa0122b21836403bda
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-15 23:39:31 -0500

    Update signoVital.controller.ts

--- src/infrastructure/controller/signoVital.controller.ts
+++ src/infrastructure/controller/signoVital.controller.ts
@@ -1,19 +1,20 @@
 import { Request, Response } from 'express';
-import { IRepositorioSignoVital } from '../../domain/interface/repositorio.interface';
-import { SignoVital } from '../repository/signoVital.repository';
+import { SignoVitalRepositorio } from '../repository/signoVital.repository';
 import {handleHttp} from '../../util/error.until';
 import { generarMensajeError, generarMensajeSatisfactorio } from '../../util/mensaje.until';
+import { SignoVitalService } from '../../app/service/signoVital.service';
 
 export class SignoVitalController{
-    private repositorioAtributo:IRepositorioSignoVital
+    private service:SignoVitalService
 
-    constructor(repositorio:IRepositorioSignoVital=new SignoVital()) {
-        this.repositorioAtributo=repositorio
+    constructor() {
+        const repositorio=new SignoVitalRepositorio()
+        this.service=new SignoVitalService(repositorio)
     }
 
     controladorObtenerSignos=(req:Request,res:Response)=>{
         try {
-            let signo=this.repositorioAtributo.obtenerSignos()
+            let signo=this.service.obtenerSignos()
             res.json(signo)
         } catch (error) {
             handleHttp(res.status(404),'ERROR_EN_OBTENER_SIGNOS',error)
@@ -32,7 +33,7 @@ export class SignoVitalController{
                 })
             }
 
-            let SoloSigno=this.repositorioAtributo.obtenerUnSigno(buscarCodigo)
+            let SoloSigno=this.service.obtenerUnSigno(buscarCodigo)
 
     
             if (!SoloSigno) {
@@ -51,7 +52,7 @@ export class SignoVitalController{
 
     controladorCrearSigno=(req:Request,res:Response)=>{
         try {
-            let crearSigno=this.repositorioAtributo.crearSigno(req.body)
+            let crearSigno=this.service.crearSigno(req.body)
             res.status(201).json(crearSigno)
         } catch (error) {
             handleHttp(res,'ERROR_CREAR_SIGNO',error)
@@ -70,7 +71,7 @@ export class SignoVitalController{
                 })
             }
 
-            let actualizarSigno=this.repositorioAtributo.actualizarSigno(buscarCodigo,req.body)
+            let actualizarSigno=this.service.actualizarSigno(buscarCodigo,req.body)
     
             if (!actualizarSigno) {
                 res.status(404).json(
@@ -95,7 +96,7 @@ export class SignoVitalController{
                 })
             }
             
-            let eliminarSigno=this.repositorioAtributo.eliminarSigno(buscarCodigo)
+            let eliminarSigno=this.service.eliminarSigno(buscarCodigo)
 
             if(eliminarSigno){
                 res.status(200).json(
