ba30e1696dab5992041637ade02d65dc847437a7
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-15 20:22:17 -0500

    Formateo.

--- src/data/signoVital.data.ts
+++ src/data/signoVital.data.ts
@@ -1,34 +1,34 @@
 import { ISignoVital } from "../interface/signoVital.interface";
 
-const SV:ISignoVital[]=[
+const SV: ISignoVital[] = [
     {
-    codigo:1,
-    descripcion:"Presión arterial"
+        codigo: 1,
+        descripcion: "Presión arterial"
     },
     {
-        codigo:2,
-        descripcion:"Frecuencia cardíaca"
+        codigo: 2,
+        descripcion: "Frecuencia cardíaca"
     },
     {
-        codigo:3,
-        descripcion:"Frecuencia respiratoria"
-    },{
-        codigo:4,
-        descripcion:"Temperatura"
+        codigo: 3,
+        descripcion: "Frecuencia respiratoria"
+    }, {
+        codigo: 4,
+        descripcion: "Temperatura"
     },
     {
-        codigo:5,
-        descripcion:"Peso"
+        codigo: 5,
+        descripcion: "Peso"
     },
     {
-        codigo:6,
-        descripcion:"Dolor de cabeza"
+        codigo: 6,
+        descripcion: "Dolor de cabeza"
     }
 ]
 
-let SVEjemplo:ISignoVital={
-    codigo:10,
-    descripcion:"Presión arterial 2"
+let SVEjemplo: ISignoVital = {
+    codigo: 10,
+    descripcion: "Presión arterial 2"
 }
 
-export {SV,SVEjemplo}
+export { SV, SVEjemplo }
