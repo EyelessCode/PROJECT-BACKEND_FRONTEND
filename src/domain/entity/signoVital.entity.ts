3c2b04306c62beef72f7f52bb6e040353834d5ec
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-15 22:25:07 -0500

    Update signoVital.entity.ts

--- src/domain/entity/signoVital.entity.ts
+++ src/domain/entity/signoVital.entity.ts
@@ -1,14 +1,1 @@
-export class SignoVital {
-    constructor(
-        public codigo: number,
-        public descripcion: string
-    ) {}
-
-    // Método para actualizar la descripción del signo vital
-    actualizarDescripcion(nuevaDescripcion: string): void {
-        if (!nuevaDescripcion || nuevaDescripcion.trim().length === 0) {
-            throw new Error("La descripción no puede estar vacía.");
-        }
-        this.descripcion = nuevaDescripcion.trim();
-    }
-}
+// que carajos va acá
