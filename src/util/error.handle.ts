7aaa499903f049d6aa907886dd51d5eefe57c2d1
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-15 20:22:25 -0500

    Update error.handle.ts

--- src/util/error.handle.ts
+++ src/util/error.handle.ts
@@ -1,9 +1,9 @@
 import { Response } from "express";
 
-const handleHttp=(res:Response,error:string,errorBruto:any)=>{
-    console.log(errorBruto);
-    res.status(500)
-    res.send({error})
-}
+const handleHttp = (res: Response, error: string, errorBruto: any) => {
+    console.error(errorBruto); // Log para depuración
+    res.status(500).json({ error });
+};
+
 
 export {handleHttp};
