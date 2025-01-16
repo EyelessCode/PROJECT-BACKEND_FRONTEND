b9da0181d09259b19a5f8dc22ece27375502cba3
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-15 20:22:29 -0500

    Update mensaje.handle.ts

--- src/util/mensaje.handle.ts
+++ src/util/mensaje.handle.ts
@@ -1,13 +1,9 @@
 const generarMensajeSatisfactorio=()=>{
-    return{
         message:"Petición exitosa"
-    }
 }
 
 const generarMensajeError=()=>{
-    return{
         message:"Error en la petición"
-    }
 }
 
 export {generarMensajeError,generarMensajeSatisfactorio}
