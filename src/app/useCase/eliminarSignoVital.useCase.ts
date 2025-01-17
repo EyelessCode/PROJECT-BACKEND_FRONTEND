0725ce363b5319f365aeaec232c0709c108643c8
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-16 22:29:15 -0500

    Update eliminarSignoVital.useCase.ts

---
+++ src/app/useCase/eliminarSignoVital.useCase.ts
@@ -0,0 +1,30 @@
+import { SignoVital } from "@prisma/client";
+import { prisma } from "../../infrastructure/data/prisma.service";
+
+const eliminarSignoVitalCasoUso=async(codigo:number):Promise<SignoVital|null>=>{
+    try {
+        const existeSignoVital=await prisma.signoVital.findUnique({
+            where:{
+                codigo:codigo
+            }
+        })
+
+        if (!existeSignoVital) {
+            console.log(`Signo Vital con el código ${codigo} no existe!`);
+            return null
+        }
+
+        const eliminarSignoVital=await prisma.signoVital.delete({
+            where:{
+                codigo:codigo
+            },
+        })
+
+        return eliminarSignoVital
+    } catch (error) {
+        console.error(`Error al tratar de eliminar un Signo Vital`);
+        throw new Error(`No se pudo eliminar un Signo Vital`)
+    }
+}
+
+export {eliminarSignoVitalCasoUso}
