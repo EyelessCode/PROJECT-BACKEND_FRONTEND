ca7f6006c3cc5dea373a0c2a8d5e81ed90bd03a0
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-16 21:45:58 -0500

    Delete signoVital.repository.interface.ts

--- src/domain/interface/signoVital/signoVital.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ISignoVital } from '../signoVital/signoVital.interface';
-
-export interface ISignoVitalRepositorio{
-    obtenerSignos():ISignoVital[]
-    obtenerUnSigno(codigo:number):ISignoVital|undefined
-    crearSigno(signo:ISignoVital):ISignoVital
-    actualizarSigno(codigo:number,signo:ISignoVital):ISignoVital|undefined
-    eliminarSigno(codigo:number):boolean
-}
