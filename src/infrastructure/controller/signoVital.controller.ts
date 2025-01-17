04f75c092032d8d9ad73bf168bf411714f74b6e3
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-16 23:02:56 -0500

    Update signoVital.controller.ts

--- src/infrastructure/controller/signoVital.controller.ts
+++ src/infrastructure/controller/signoVital.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { crearSignoVital } from "../../app/useCase/signoVital/crearSignoVital.useCase";
+import { actualizarSignoVital } from "../../app/useCase/signoVital/actualizarSignoVital.useCase";
 
 export class SignoVitalController{
     async controladorCrearSigno(req:Request,res:Response){
@@ -11,6 +12,32 @@ export class SignoVitalController{
             console.error(error);
             res.status(500).json({
                 message:"Error al crear el signo vital"
+            })
+        }
+    }
+
+    async controladorActualizarSigno(req:Request,res:Response) {
+        try {
+            const index=parseInt(req.params.codigo)
+            const data=req.body
+
+            const signoVitalActualizado=await actualizarSignoVital(index,data)
+            res.status(201).json(signoVitalActualizado)
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                message:"Error al actualizar el signo vital"
+            })
+        }
+    }
+
+    async controladorEliminarSigno(req:Request,res:Response){
+        try {
+            
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                message:"Error al eliminar el signo vital"
             })
         }
     }
