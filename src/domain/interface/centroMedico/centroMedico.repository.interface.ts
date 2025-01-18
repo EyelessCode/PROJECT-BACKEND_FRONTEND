7b39b52b85924196e223f41b1f5ccae31dc96a80
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 15:06:53 -0500

    Delete centroMedico.repository.interface.ts

--- src/domain/interface/centroMedico/centroMedico.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { ICentroMedico } from "./centroMedico.interface";
-
-export interface ICentroMedicoRepositorio{
-    obtenerCentrosMedicos():ICentroMedico[]
-    // obtenerUnCentroMedico(codigo:number):ICentroMedico|undefined
-}
