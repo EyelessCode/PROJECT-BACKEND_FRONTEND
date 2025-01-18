34578d4bd1e84b298ec78fcfd97f1fb273ae537a
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 11:05:58 -0500

    Update validaciones.middleware.ts

---
+++ src/infrastructure/validaciones.middleware.ts
@@ -0,0 +1,21 @@
+// import {} from 'express-validator'
+
+import { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+const validarSignoVital=[
+    body("descripcion").isString().withMessage("La descripción debe de ser un texto"),
+    body("unidad").isString().withMessage("La unidad debe de ser texto"),
+    body("valorMinimo").isFloat({gt:0}).withMessage("El valor mínimo debe ser un número positivo"),
+    body("valorMaximo").isFloat({gt:0}).withMessage("El valor máximo debe ser un número positivo"),
+    (req:Request,res:Response,next:any)=>{
+        const errores=validationResult(req)
+
+        if (!errores.isEmpty()) {
+            return res.status(400).json({
+                errores:errores.array()
+            })
+        }
+        next()
+    }
+]
