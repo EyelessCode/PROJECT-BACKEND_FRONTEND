4da5eabedef24b91e525c0a1eca03af73d48806e
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 12:37:16 -0500

    Update obtenerPaciente.useCase.ts

---
+++ src/app/useCase/paciente/obtenerPaciente.useCase.ts
@@ -0,0 +1,25 @@
+import { Paciente } from "@prisma/client";
+import { IPaciente } from "../../../domain/interface/paciente/paciente.interface";
+import { prisma } from "../../../infrastructure/data/prisma.service";
+
+const obtenerPaciente=async(codigo:number,data:IPaciente):Promise<Paciente|null>=>{
+    try {
+        const existePaciente=await prisma.paciente.findUnique({
+            where:{
+                codigo:codigo
+            }
+        })
+    
+        if (!existePaciente) {
+            console.log(`No existe un paciente con el código ${codigo}`);
+            return null
+        }
+
+        return existePaciente
+    } catch (error) {
+        console.error(`Error al obtener un Paciente con el código: ${codigo}`);
+        throw new Error(`No se pudo obtener un Paciente con el código: ${codigo}`);
+    }
+}
+
+export {obtenerPaciente}
