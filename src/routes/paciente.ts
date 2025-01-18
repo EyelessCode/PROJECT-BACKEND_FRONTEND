268c7c49e106a2b113813dc89d1eab647b06acef
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 16:04:16 -0500

    Me faltó los 'códigos'.

--- src/routes/paciente.ts
+++ src/routes/paciente.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { PacienteController } from "../infrastructure/controller/paciente.controller";
+import { validarPaciente } from "../infrastructure/validation/validation.middleware";
 
 const ruta=Router()
 const controlador=new PacienteController()
 
-ruta.get('/')
+ruta.get('/',controlador.controladorObtenerPacientes.bind(controlador))
+ruta.get('/:codigo',controlador.controladorObtenerPaciente.bind(controlador))
+ruta.post('/',validarPaciente,controlador.controladorCrearPaciente.bind(controlador))
+ruta.put('/:codigo',validarPaciente,controlador.controladorActualizarPaciente.bind(controlador))
+ruta.delete('/:codigo',controlador.controladorEliminarPaciente.bind(controlador))
 
 export {ruta}
