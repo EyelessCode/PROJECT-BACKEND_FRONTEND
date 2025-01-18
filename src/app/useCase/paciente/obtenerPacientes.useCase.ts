83d1e6dae1c6b2b596fdb91f92f42618098dc607
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 15:06:41 -0500

    Update obtenerPacientes.useCase.ts

---
+++ src/app/useCase/paciente/obtenerPacientes.useCase.ts
@@ -0,0 +1,18 @@
+import { Paciente } from "@prisma/client";
+import { prisma } from "../../../infrastructure/data/prisma.service";
+
+const obtenerPacientes=async():Promise<Paciente[]|null>=>{
+    try {
+        const obtenerPacientes=await prisma.paciente.findMany()
+
+        if (!obtenerPacientes) {
+            console.log(`No existen Signos Vitales`);
+            return null
+        }
+
+        return obtenerPacientes
+    } catch (error) {
+        console.error(`Error al obtener todos los Pacientes, ${error}`);
+        throw new Error(`No se pudo obtener los Pacientes`)
+    }
+}
