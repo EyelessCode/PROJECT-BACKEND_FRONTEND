0019a28bb1f9b2878639a8200c49bc62c1e1e499
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 15:06:45 -0500

    Update obtenerSignosVitales.caseUse.ts

--- src/app/useCase/signoVital/obtenerSignosVitales.caseUse.ts
+++ src/app/useCase/signoVital/obtenerSignosVitales.caseUse.ts
@@ -1,9 +1,14 @@
 import { SignoVital } from "@prisma/client";
 import { prisma } from "../../../infrastructure/data/prisma.service";
 
-const obtenerSignosVitalesCasoUso=async():Promise<SignoVital[]>=>{
+const obtenerSignosVitalesCasoUso=async():Promise<SignoVital[]|null>=>{
     try {
         const obtenerSignosVitales=await prisma.signoVital.findMany()
+
+        if (!obtenerSignosVitales) {
+            console.log(`No existen Signos Vitales`);
+            return null
+        }
 
         return obtenerSignosVitales
     } catch (error) {
