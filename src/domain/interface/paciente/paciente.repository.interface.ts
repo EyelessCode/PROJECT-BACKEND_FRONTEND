ae61c9e33401e7f9466ab4b127c00353b060b26b
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 11:27:49 -0500

    Delete paciente.repository.interface.ts

--- src/domain/interface/paciente/paciente.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { IPaciente } from "./paciente.interface"
-
-export interface IPacienteRepositorio{
-    obtenerPacientes():IPaciente[]
-    obtenerUnPaciente(codigo:number):IPaciente|undefined
-    crearPaciente(paciente:IPaciente):IPaciente
-    actualizarPaciente(codigo:number,paciente:IPaciente):IPaciente|undefined
-    eliminarPaciente(codigo:number):boolean
-}
