c0f5d70a129e97af223936fa2580c358ab18c3d0
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 18:05:09 -0500

    Update enfermera.repository.ts

---
+++ src/infrastructure/repository/enfermera.repository.ts
@@ -0,0 +1,62 @@
+import { Enfermera } from "@prisma/client";
+import { IEnfermeraRepositorio } from "../../domain/interface/enfermera/enfermera.repository.interface";
+import { prisma } from "../data/prisma.service";
+
+export class EnfermeraRepositorio implements IEnfermeraRepositorio{
+    async crearEnfermera(data: Enfermera): Promise<Enfermera> {
+        // throw new Error("Method not implemented.");
+        return await prisma.enfermera.create({
+            data:data
+        })
+    }
+
+    async obtenerEnfermera(codigo: number): Promise<Enfermera | null> {
+        // throw new Error("Method not implemented.");
+
+        if (codigo<=0) {
+            throw new Error("El código del centro médico debe ser mayor a 0");
+        }
+
+        return await prisma.enfermera.findUnique({
+            where:{
+                codigo:codigo
+            }
+        })
+    }
+
+    async obtenerEnfermeras(): Promise<Enfermera[]> {
+        // throw new Error("Method not implemented.");
+
+        return await prisma.enfermera.findMany()
+    }
+
+    async actualizarEnfermera(codigo: number, data: Partial<Enfermera>): Promise<Enfermera | null> {
+        // throw new Error("Method not implemented.");
+
+        if (codigo<=0) {
+            throw new Error("El código del centro médico debe ser mayor a 0");
+        }
+
+        return await prisma.enfermera.update({
+            where:{
+                codigo:codigo
+            },
+            data:data
+        }) 
+    }
+
+    async eliminarEnfermera(codigo: number): Promise<Enfermera | null> {
+        // throw new Error("Method not implemented.");
+
+        if (codigo<=0) {
+            throw new Error("El código del centro médico debe ser mayor a 0");
+        }
+
+        return await prisma.enfermera.delete({
+            where:{
+                codigo:codigo
+            }
+        })
+    }
+
+}
