9ce71ba15f2369d99821aecf5f53e5c40c296a02
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 16:55:12 -0500

    Update actualizarPaciente.useCase.ts

--- src/app/useCase/paciente/actualizarPaciente.useCase.ts
+++ src/app/useCase/paciente/actualizarPaciente.useCase.ts
@@ -1,21 +1,24 @@
 import { Paciente } from "@prisma/client";
 import { IPaciente } from "../../../domain/interface/paciente/paciente.interface";
-import { prisma } from "../../../infrastructure/data/prisma.service";
+// import { prisma } from "../../../infrastructure/data/prisma.service";
+import { PacienteRepositorio } from "../../../infrastructure/repository/paciente.repository";
+
+const repositorio=new PacienteRepositorio()
 
 const actualizarPaciente=async(codigo:number,data:IPaciente):Promise<Paciente|null>=>{
     try {
-        const existePaciente=await prisma.paciente.findUnique({
+        /* const existePaciente=await prisma.paciente.findUnique({
             where:{
                 codigo:codigo
             }
-        })
+        }) */
 
-        if (!existePaciente) {
+/*         if (!existePaciente) {
             console.log(`Paciente ${codigo} no encontrado`);
             return null
-        }
+        } */
 
-        const actualizarPaciente=await prisma.paciente.update({
+/*         const actualizarPaciente=await prisma.paciente.update({
             where:{
                 codigo:codigo
             },
@@ -24,9 +27,9 @@ const actualizarPaciente=async(codigo:number,data:IPaciente):Promise<Paciente|nu
                 fechaNacimiento:data.fechaNacimiento,
                 nombres:data.nombres
             }
-        })
+        }) */
 
-        return actualizarPaciente
+        return await repositorio.actualizarPaciente(codigo,data)
     } catch (error) {
         console.error(`Error al actualizar un Paciente: ${error}`);
         throw new Error('No se pudo actualizar un Paciente')
