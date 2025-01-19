841e69d2271744afcc3f1cf86f87a78c3912f897
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 20:15:01 -0500

    nuevo jest

--- src/infrastructure/controller/paciente.controller.ts
+++ src/infrastructure/controller/paciente.controller.ts
@@ -4,18 +4,21 @@ import { actualizarPaciente } from "../../app/useCase/paciente/actualizarPacient
 import { eliminarPaciente } from "../../app/useCase/paciente/eliminarPaciente.useCase";
 import { obtenerPaciente } from "../../app/useCase/paciente/obtenerPaciente.useCase";
 import { obtenerPacientes } from "../../app/useCase/paciente/obtenerPacientes.useCase";
+import { logger } from "../log/logger.service";
 
 export class PacienteController{
     async controladorCrearPaciente(req:Request,res:Response):Promise<any>{
         try {
             const data=req.body
             const nuevoPaciente=await crearPaciente(data)
+            logger.info(`PACIENTE CREADO ${nuevoPaciente.cedula}`)
             return res.status(201).json(nuevoPaciente)
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({
-                message:"Error al crear un Paciente",
-                error: error
+                console.error(error);
+                logger.error(`ERROR AL CREAR AL PACIENTE. ${error}`)
+                return res.status(500).json({
+                    message:"Error al crear un Paciente",
+                    error: error,
             })
         }
     }
@@ -40,9 +43,12 @@ export class PacienteController{
                 })
             }
 
+            logger.info(`PACIENTE ACTUALIZADO ${pacienteActualizado.cedula}`)
+
             return res.status(200).json(pacienteActualizado)
         } catch (error) {
             console.error(error);
+            logger.error(`ERROR AL ACTUALIZAR AL PACIENTE. ${error}`)
             return res.status(500).json({
                 message:"Error al actualizar el Paciente",
                 error: error
@@ -70,9 +76,12 @@ export class PacienteController{
                 })
             }
 
+            logger.info(`PACIENTE ELIMINADO ${pacienteEliminado.cedula}`)
+
             return res.status(200).json(pacienteEliminado)
         } catch (error) {
             console.error(error);
+            logger.error(`ERROR AL ELIMINAR PACIENTE. ${error}`)
             return res.status(500).json({
                 message:"Error al eliminar el Paciente",
                 error: error
@@ -100,9 +109,12 @@ export class PacienteController{
                 })
             }
 
+            logger.info(`PACIENTE FILTRADO ${paciente.cedula}`)
+
             return res.status(200).json(paciente)
         } catch (error) {
             console.error(error);
+            logger.error(`ERROR AL OBTENER PACIENTE. ${error}`)
             return res.status(500).json({
                 message:"Error al obtener el Paciente",
                 error: error
@@ -116,9 +128,12 @@ export class PacienteController{
             // const data=req.body
 
             const pacientes=await obtenerPacientes()
+
+            logger.info(`\nLISTADO DE PACIENTES\n`)
             return res.status(200).json(pacientes)
         } catch (error) {
             console.error(error);
+            logger.error(`ERROR AL OBTENER LOS PACIENTES. ${error}`)
             return res.status(500).json({
                 message:"Error al obtener los Pacientes",
                 error: error
