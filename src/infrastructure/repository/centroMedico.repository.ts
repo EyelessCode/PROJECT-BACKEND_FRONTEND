613f658c518106eb604cb5af0044aaa30eb651d1
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 17:49:04 -0500

    Update centroMedico.repository.ts

--- src/infrastructure/repository/centroMedico.repository.ts
+++ src/infrastructure/repository/centroMedico.repository.ts
@@ -1,25 +1,62 @@
 import { CentroMedico } from "@prisma/client";
 import { ICentroMedicoRepositorio } from "../../domain/interface/centroMedico/centroMedico.repository.interface";
+import { prisma } from "../data/prisma.service";
 
 export class CentroMedicoRepositorio implements ICentroMedicoRepositorio{
-    crearCentroMedico(data: CentroMedico): Promise<CentroMedico> {
-        throw new Error("Method not implemented.");
+    async crearCentroMedico(data: CentroMedico): Promise<CentroMedico> {
+        // throw new Error("Method not implemented.");
+
+        return await prisma.centroMedico.create({
+            data:data
+        })
     }
 
-    obtenerCentroMedico(codigo: number): Promise<CentroMedico | null> {
-        throw new Error("Method not implemented.");
+    async obtenerCentroMedico(codigo: number): Promise<CentroMedico | null> {
+        // throw new Error("Method not implemented.");
+
+        if (codigo<=0) {
+            throw new Error("El código del centro médico debe ser mayor a 0");
+        }
+
+        return await prisma.centroMedico.findUnique({
+            where:{
+                codigo:codigo
+            }
+        })
     }
 
-    obtenerCentrosMedicos(): Promise<CentroMedico[]> {
-        throw new Error("Method not implemented.");
+    async obtenerCentrosMedicos(): Promise<CentroMedico[]> {
+        // throw new Error("Method not implemented.");
+        return await prisma.centroMedico.findMany()
     }
 
-    actualizarCentroMedico(codigo: number, data: Partial<CentroMedico>): Promise<CentroMedico | null> {
-        throw new Error("Method not implemented.");
+    async actualizarCentroMedico(codigo: number, data: Partial<CentroMedico>): Promise<CentroMedico | null> {
+        // throw new Error("Method not implemented.");
+
+        if (codigo<=0) {
+            throw new Error("El código del centro médico debe ser mayor a 0");
+        }
+
+        return await prisma.centroMedico.update({
+            where:{
+                codigo:codigo
+            },
+            data:data
+        })
     }
-    
-    eliminarCentroMedico(codigo: number): Promise<CentroMedico | null> {
-        throw new Error("Method not implemented.");
+
+    async eliminarCentroMedico(codigo: number): Promise<CentroMedico | null> {
+        // throw new Error("Method not implemented.");
+
+        if (codigo<=0) {
+            throw new Error("El código del centro médico debe ser mayor a 0");
+        }
+        
+        return await prisma.centroMedico.delete({
+            where:{
+                codigo:codigo
+            }
+        })
     }
 
 
