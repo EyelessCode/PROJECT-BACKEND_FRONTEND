73b57dc170e57232fcdaf9fe7dace766482bf531
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 20:29:06 -0500

    Update crearPaciente.test.ts

--- src/app/useCase/paciente/__tests__/crearPaciente.test.ts
+++ src/app/useCase/paciente/__tests__/crearPaciente.test.ts
@@ -1,17 +1,22 @@
 import { crearPaciente } from "../crearPaciente.useCase";
+import { prisma } from "../../../../infrastructure/data/prisma.service";
+
+beforeEach(async () => {
+    await prisma.paciente.deleteMany();
+});
 
 describe("Caso de uso: Crear Paciente", () => {
     it("Debería crear un paciente correctamente", async () => {
         const data = {
             cedula: "0987654321",
             nombres: "Carlos García",
-            fechaNacimiento:"1995-06-15",
+            fechaNacimiento: "1995-06-15",
         };
 
         const paciente = await crearPaciente(data);
 
         expect(paciente).toHaveProperty("cedula", "0987654321");
         expect(paciente).toHaveProperty("nombres", "Carlos García");
-        expect(paciente.fechaNacimiento).toEqual(new Date("1995-06-15"));
+        expect(paciente.fechaNacimiento).toBe("1995-06-15")
     });
 });
