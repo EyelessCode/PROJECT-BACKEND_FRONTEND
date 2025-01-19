547802ec6dea584d38aa1221c9eaf4f29f34b306
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 11:51:21 -0500

    Update crearCentroMedico.useCase.ts

---
+++ src/app/useCase/centroMedico/crearCentroMedico.useCase.ts
@@ -0,0 +1,21 @@
+import { CentroMedico } from "@prisma/client";
+import { ICentroMedico } from "../../../domain/interface/centroMedico/centroMedico.interface";
+import { prisma } from "../../../infrastructure/data/prisma.service";
+
+const crearCentroMedico=async(data:ICentroMedico):Promise<CentroMedico>=>{
+    try {
+        const crear=await prisma.centroMedico.create({
+            data:{
+                nombre:data.nombre,
+                direccion:data.direccion
+            }
+        })
+
+        return crear
+    } catch (error) {
+        console.error(`Error al crear un nuevo Paciente: ${error}`);
+        throw new Error('No se pudo crear un nuevo Paciente')
+    }
+}
+
+export {crearCentroMedico}
