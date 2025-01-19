d7887393ff41586d6f54f12036a6e87773573cfa
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 12:36:37 -0500

    Update centroMedico.controller.ts

--- src/infrastructure/controller/centroMedico.controller.ts
+++ src/infrastructure/controller/centroMedico.controller.ts
@@ -1,43 +1,129 @@
 import { Request, Response } from "express";
+import { crearCentroMedico } from "../../app/useCase/centroMedico/crearCentroMedico.useCase";
+import { eliminarCentroMedico } from "../../app/useCase/centroMedico/eliminarCentroMedico.useCase";
+import { actualizarCentroMedico } from "../../app/useCase/centroMedico/actualizarCentroMedico.useCase";
+import { obtenerCentroMedico } from "../../app/useCase/centroMedico/obtenerCentroMedico.useCase";
+import { obtenerCentrosMedicos } from "../../app/useCase/centroMedico/obtenerCentrosMedicos.useCase";
 
 export class CentroMedicoController{
     async controladorCrearCentroMedico(req:Request,res:Response):Promise<any>{
         try {
-            
+            const data=req.body
+            const crear=crearCentroMedico(data)
+
+            return res.status(201).json(crear)
         } catch (error) {
-            
+            console.error(error);
+            // logger.error(`ERROR AL CREAR AL CENTRO MÉDICO. ${error}`)
+            return res.status(500).json({
+                message:"Error al crear un Centro Médico",
+                error: error,
+            })
         }
     }
 
     async controladorEliminarCentroMedico(req:Request,res:Response):Promise<any>{
         try {
+            const codigo=parseInt(req.params.codigo)
             
+            if (isNaN(codigo)) {
+                return res.status(400).json({
+                    message: "El código no es un número"
+                })
+            }
+            // const data=req.body
+
+            const eliminar=eliminarCentroMedico(codigo)
+
+            if (!eliminar) {
+                return res.status(404).json({
+                    message: `El Centro Médico: ${codigo} no existe`
+                })
+            }
+
+            return res.status(200).json(eliminar)
         } catch (error) {
-            
+            console.error(error);
+            // logger.error(`ERROR AL CREAR AL CENTRO MÉDICO. ${error}`)
+            return res.status(500).json({
+                message:"Error al eliminar un Centro Médico",
+                error: error,
+            })
         }
     }
 
     async controladorActualizarCentroMedico(req:Request,res:Response):Promise<any>{
         try {
+            const codigo=parseInt(req.params.codigo)
             
+            if (isNaN(codigo)) {
+                return res.status(400).json({
+                    message: "El código no es un número"
+                })
+            }
+            const data=req.body
+
+            const actualizar=actualizarCentroMedico(codigo,data)
+
+            if (!actualizar) {
+                return res.status(404).json({
+                    message: `El Centro Médico: ${codigo} no existe`
+                })
+            }
+
+            return res.status(200).json(actualizar)
+
         } catch (error) {
-            
+            console.error(error);
+            // logger.error(`ERROR AL CREAR AL CENTRO MÉDICO. ${error}`)
+            return res.status(500).json({
+                message:"Error al actualizar un Centro Médico",
+                error: error,
+            })
         }
     }
 
     async controladorObtenerCentroMedico(req:Request,res:Response):Promise<any>{
         try {
+            const codigo=parseInt(req.params.codigo)
             
+            if (isNaN(codigo)) {
+                return res.status(400).json({
+                    message: "El código no es un número"
+                })
+            }
+
+            const obtener=obtenerCentroMedico(codigo)
+
+            if (!obtener) {
+                return res.status(404).json({
+                    message: `El Centro Médico: ${codigo} no existe`
+                })
+            }
+
+            return res.status(200).json(obtener)
         } catch (error) {
-            
+            console.error(error);
+            // logger.error(`ERROR AL CREAR AL CENTRO MÉDICO. ${error}`)
+            return res.status(500).json({
+                message:"Error al obtener un Centro Médico",
+                error: error,
+            })
         }
     }
-    
-    async controladorObtenerCentrosMedicos(req:Request,res:Response):Promise<any>{
+
+    async controladorObtenerCentrosMedicos(res:Response):Promise<any>{
         try {
-            
+            const obtener=obtenerCentrosMedicos()
+
+            return res.status(200).json(obtener)
         } catch (error) {
-            
+            console.error(error);
+            // logger.error(`ERROR AL CREAR AL CENTRO MÉDICO. ${error}`)
+            return res.status(500).json({
+                message:"Error al obtener los Centros Médicos",
+                error: error,
+            })
         }
     }
 }
