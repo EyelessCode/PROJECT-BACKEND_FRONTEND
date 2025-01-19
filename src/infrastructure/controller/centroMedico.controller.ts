8d9e31fe51788fb5cdfb9debcae29cd2679c85ac
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 12:09:24 -0500

    Update centroMedico.controller.ts

---
+++ src/infrastructure/controller/centroMedico.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+
+export class CentroMedicoController{
+    async controladorCrearCentroMedico(req:Request,res:Response):Promise<any>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async controladorEliminarCentroMedico(req:Request,res:Response):Promise<any>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async controladorActualizarCentroMedico(req:Request,res:Response):Promise<any>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async controladorObtenerCentroMedico(req:Request,res:Response):Promise<any>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+    
+    async controladorObtenerCentrosMedicos(req:Request,res:Response):Promise<any>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
