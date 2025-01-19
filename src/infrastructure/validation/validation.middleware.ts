3e9e83894f736139799959ad746078d16d56278a
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 13:17:22 -0500

    Mejores validaciones

--- src/infrastructure/validation/validation.middleware.ts
+++ src/infrastructure/validation/validation.middleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
 const validarSignoVital:any=[
-    body("descripcion").isString().withMessage("La descripción debe de ser un texto"),
+    body("descripcion").isString().withMessage("La descripción debe de ser un texto").isLength({max:120}),
     body("unidad").isString().withMessage("La unidad debe de ser texto"),
     body("valorMinimo").isFloat({gt:0}).withMessage("El valor mínimo debe ser un número positivo"),
     body("valorMaximo").isFloat({gt:0}).withMessage("El valor máximo debe ser un número positivo"),
@@ -21,8 +21,8 @@ const validarSignoVital:any=[
 ]
 
 const validarPaciente:any=[
-    body("cedula").isString().withMessage("La cédula debe de ser un texto"),
-    body("nombres").isString().withMessage("Los nombres deben de ser texto"),
+    body("cedula").isString().withMessage("La cédula debe de ser un texto").isLength({max:10}),
+    body("nombres").isString().withMessage("Los nombres deben de ser texto").isLength({min:3,max:60}),
     body("fechaNacimiento").isString().withMessage("La fecha de nacimiento debe ser válida")
     .matches(/^\d{4}-\d{2}-\d{2}$/).withMessage("Formato de fecha inválido (YYYY-MM-DD)"),
     (req:Request,res:Response,next:NextFunction)=>{
@@ -38,7 +38,7 @@ const validarPaciente:any=[
 ]
 
 const validarCentroMedico:any=[
-    body('nombre').isString().withMessage("El nombre debe ser texto"),
+    body('nombre').isString().withMessage("El nombre debe ser texto").isLength({max:85}),
     body('direccion').isString().withMessage("La dirección debe ser texto"),
     (req:Request,res:Response,next:NextFunction)=>{
         const errores=validationResult(req)
