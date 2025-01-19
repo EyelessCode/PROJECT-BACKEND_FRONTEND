43a5d34e98c90f690ac3d4f275da4bf6e687d5fc
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 16:44:50 -0500

    Update crearPaciente.useCase.ts

--- src/app/useCase/paciente/crearPaciente.useCase.ts
+++ src/app/useCase/paciente/crearPaciente.useCase.ts
@@ -1,18 +1,21 @@
 import { Paciente } from "@prisma/client";
-import { IPaciente } from "../../../domain/interface/paciente/paciente.interface";
-import { prisma } from "../../../infrastructure/data/prisma.service";
+// import { IPaciente } from "../../../domain/interface/paciente/paciente.interface";
+// import { prisma } from "../../../infrastructure/data/prisma.service";
+import { PacienteRepositorio } from "../../../infrastructure/repository/paciente.repository";
 
-const crearPaciente=async(data:IPaciente):Promise<Paciente>=>{
+const repositorio=new PacienteRepositorio()
+
+const crearPaciente=async(data:Paciente):Promise<Paciente>=>{
     try {
-        const nuevoPaciente=await prisma.paciente.create({
+        /* const nuevoPaciente=await prisma.paciente.create({
             data:{
                 cedula:data.cedula,
                 fechaNacimiento:data.fechaNacimiento,
                 nombres:data.nombres
             }
-        })
+        }) */
 
-        return nuevoPaciente
+        return await repositorio.crearPaciente(data)
     } catch (error) {
         console.error(`Error al crear un nuevo Paciente: ${error}`);
         throw new Error('No se pudo crear un nuevo Paciente')
