37da93fe1b9d4097c9be64e9e0e8a96326ad6f40
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 20:29:55 -0500

    Update crearPaciente.test.ts

--- src/app/useCase/paciente/__tests__/crearPaciente.test.ts
+++ src/app/useCase/paciente/__tests__/crearPaciente.test.ts
@@ -8,15 +8,15 @@ beforeEach(async () => {
 describe("Caso de uso: Crear Paciente", () => {
     it("Debería crear un paciente correctamente", async () => {
         const data = {
-            cedula: "0987654321",
-            nombres: "Carlos García",
-            fechaNacimiento: "1995-06-15",
+            cedula: "0987452896",
+            nombres: "Pepe Gonzáles",
+            fechaNacimiento: "2004-12-12",
         };
 
         const paciente = await crearPaciente(data);
 
-        expect(paciente).toHaveProperty("cedula", "0987654321");
-        expect(paciente).toHaveProperty("nombres", "Carlos García");
-        expect(paciente.fechaNacimiento).toBe("1995-06-15")
+        expect(paciente).toHaveProperty("cedula", "0987452896");
+        expect(paciente).toHaveProperty("nombres", "Pepe Gonzáles");
+        expect(paciente.fechaNacimiento).toBe("2004-12-12")
     });
 });
