9e34e520f4c6ab0b27fa621cc410586aa0b3aee9
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 14:00:54 -0500

    Update enfermera.controller.ts

---
+++ src/infrastructure/controller/enfermera.controller.ts
@@ -0,0 +1,137 @@
+import { Request, Response } from "express";
+import { crearEnfermera } from "../../app/useCase/enfermera/crearEnfermera.useCase";
+import { eliminarEnfermera } from "../../app/useCase/enfermera/eliminarEnfermera.useCase";
+import { logger } from "../log/logger.service";
+import { actualizarEnfermeras } from "../../app/useCase/enfermera/actualizarEnfermera.useCase";
+import { obtenerEnfermera } from "../../app/useCase/enfermera/obtenerEnfermera.useCase";
+import { obtenerEnfermeras } from "../../app/useCase/enfermera/obtenerEnfermeras.useCase";
+
+export class EnfermeraController{
+    async controladorCrearEnfermera(req:Request,res:Response):Promise<any>{
+        try {
+            const data=req.body
+            const crear=await crearEnfermera(data)
+            logger.info(`ENFERMERA CREADA ${crear.cedula}`)
+
+            return res.status(201).json(crear)
+        } catch (error) {
+            console.error(error);
+            logger.error(`ERROR AL CREAR AL ENFERMERA. ${error}`)
+            return res.status(500).json({
+                message:"Error al crear una Enfermera",
+                error: error,
+            })
+        }
+    }
+
+    async controladorEliminarEnfermera(req:Request,res:Response):Promise<any>{
+        try {
+            const codigo=parseInt(req.params.codigo)
+            
+            if (isNaN(codigo)) {
+                return res.status(400).json({
+                    message: "El código no es un número"
+                })
+            }
+            // const data=req.body
+
+            
+            const eliminar=await eliminarEnfermera(codigo)
+            
+            if (!eliminar) {
+                return res.status(404).json({
+                    message: `La Enfermera: ${codigo} no existe`
+                })
+            }
+            
+            logger.info(`ENFERMERA ELIMINADA ${eliminar.cedula}`)
+            return res.status(200).json(eliminar)
+        } catch (error) {
+            console.error(error);
+            logger.error(`ERROR AL ELIMINAR ENFERMERA. ${error}`)
+            return res.status(500).json({
+                message:"Error al eliminar una Enfermera",
+                error: error,
+            })
+        }
+    }
+
+    async controladorActualizarEnfermera(req:Request,res:Response):Promise<any>{
+        try {
+            const codigo=parseInt(req.params.codigo)
+            
+            if (isNaN(codigo)) {
+                return res.status(400).json({
+                    message: "El código no es un número"
+                })
+            }
+            const data=req.body
+
+            const actualizar=await actualizarEnfermeras(codigo,data)
+
+            if (!actualizar) {
+                return res.status(404).json({
+                    message: `La Enfermera: ${codigo} no existe`
+                })
+            }
+            logger.info(`ENFERMERA ACTUALIZADA ${actualizar.cedula}`)
+
+            return res.status(200).json(actualizar)
+
+        } catch (error) {
+            console.error(error);
+            logger.error(`ERROR AL ACTUALIZAR ENFERMERA. ${error}`)
+            return res.status(500).json({
+                message:"Error al actualizar una Enfermera",
+                error: error,
+            })
+        }
+    }
+
+    async controladorObtenerEnfermera(req:Request,res:Response):Promise<any>{
+        try {
+            const codigo=parseInt(req.params.codigo)
+            
+            if (isNaN(codigo)) {
+                return res.status(400).json({
+                    message: "El código no es un número"
+                })
+            }
+
+            const obtener=await obtenerEnfermera(codigo)
+
+            if (!obtener) {
+                return res.status(404).json({
+                    message: `La Enfermera: ${codigo} no existe`
+                })
+            }
+            logger.info(`ENFERMERA OBTENIDA ${obtener.cedula}`)
+
+            return res.status(200).json(obtener)
+        } catch (error) {
+            console.error(error);
+            logger.error(`ERROR AL CREAR AL ENFERMERA. ${error}`)
+            return res.status(500).json({
+                message:"Error al obtener una Enfermera",
+                error: error,
+            })
+        }
+    }
+
+    async controladorObtenerEnfermeras(req:Request,res:Response):Promise<any>{
+        try {
+            const obtener=await obtenerEnfermeras()
+
+            logger.info(`\nLISTADO DE PACIENTES\n`)
+
+            return res.status(200).json(obtener)
+        } catch (error) {
+            console.error(error);
+            logger.error(`ERROR AL OBTENER LAS ENFERMERAS. ${error}`)
+            return res.status(500).json({
+                message:"Error al obtener las Enfermeras",
+                error: error,
+            })
+        }
+    }
+}
