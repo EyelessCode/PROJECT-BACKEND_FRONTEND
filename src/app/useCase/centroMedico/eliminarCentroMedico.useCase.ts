c0745d93ef7b9e12f5c47563006968bf02727abf
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 12:00:47 -0500

    Update eliminarCentroMedico.useCase.ts

---
+++ src/app/useCase/centroMedico/eliminarCentroMedico.useCase.ts
@@ -0,0 +1,28 @@
+import { CentroMedico } from "@prisma/client";
+import { prisma } from "../../../infrastructure/data/prisma.service";
+
+const eliminarCentroMedico=async(codigo:number):Promise<CentroMedico|null>=>{
+    try {
+        const existe=await prisma.centroMedico.findUnique({
+            where:{
+                codigo:codigo
+            }
+        })
+
+        if (!existe) {
+            console.log(`No existe un Centro Médico con el código ${codigo}`);
+            return null
+        }
+
+        const eliminar=await prisma.centroMedico.delete({
+            where:{
+                codigo:codigo
+            }
+        })
+
+        return eliminar
+    } catch (error) {
+        console.error(`Error al crear eliminar Centro Médico: ${error}`);
+        throw new Error('No se pudo eliminar un Centro Médico')
+    }
+}
