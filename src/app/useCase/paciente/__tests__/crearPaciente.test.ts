24fd16b865d1c94d15c838938c771338482d8f56
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-18 20:58:58 -0500

    Update crearPaciente.test.ts

--- src/app/useCase/paciente/__tests__/crearPaciente.test.ts
+++ src/app/useCase/paciente/__tests__/crearPaciente.test.ts
@@ -19,7 +19,7 @@ describe("Caso de uso: Crear Paciente", () => {
         expect(paciente).toHaveProperty("nombres", "Pepe Gonzáles");
         expect(paciente.fechaNacimiento).toBe("2004-12-12")
 
-/*         const pacienteDB = await prisma.paciente.findUnique({
+        /* const pacienteDB = await prisma.paciente.findUnique({
             where: { cedula: "0987654321" },
         });
         expect(pacienteDB).not.toBeNull(); */
