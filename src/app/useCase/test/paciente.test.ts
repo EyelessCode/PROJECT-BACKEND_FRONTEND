bdc2638097e20cd95e0088893e912d8200d1d9ce
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 22:49:50 -0500

    Update paciente.test.ts

--- src/app/useCase/test/paciente.test.ts
+++ src/app/useCase/test/paciente.test.ts
@@ -2,24 +2,32 @@ import { crearPaciente,actualizarPaciente, eliminarPaciente, obtenerPaciente } f
 import { prisma } from "../../../infrastructure/data/prisma.service";
 
 beforeEach(async () => {
-    await prisma.paciente.deleteMany();
+    await prisma.paciente.deleteMany(); // Limpia la base de datos antes de cada test
+    await prisma.paciente.create({
+        data: {
+            codigo: 45,
+            cedula: "0987452896",
+            nombres: "Pepe Gonzáles",
+            fechaNacimiento: "2004-12-12",
+        },
+    });
 });
 
 describe("Caso de uso: Crear Paciente", () => {
     it("Debería CREAR un paciente correctamente", async () => {
         const data = {
-            codigo:45,
-            cedula: "0987452896",
-            nombres: "Pepe Gonzáles",
-            fechaNacimiento: "2004-12-12",
+            codigo:46,
+            cedula: "0987452897",
+            nombres: "Juan Pérez",
+            fechaNacimiento: "2003-11-10",
         };
 
         const paciente = await crearPaciente(data);
 
         // expect(codigo).toHaveProperty("codigo", 45);
-        expect(paciente).toHaveProperty("cedula", "0987452896");
-        expect(paciente).toHaveProperty("nombres", "Pepe Gonzáles");
-        expect(paciente.fechaNacimiento).toBe("2004-12-12")
+        expect(paciente).toHaveProperty("cedula", "0987452897");
+        expect(paciente).toHaveProperty("nombres", "Juan Pérez");
+        expect(paciente.fechaNacimiento).toBe("2003-11-10")
 
         /* const pacienteDB = await prisma.paciente.findUnique({
             where: { cedula: "0987654321" },
@@ -36,16 +44,16 @@ describe("Caso de uso: Actualizar Paciente", () => {
         const data = {
             codigo:40,
             cedula: "0999999999",
-            nombres: "Pero Gonzáles",
-            fechaNacimiento: "2004-12-01",
+            nombres: "Carlos Ramírez",
+            fechaNacimiento: "2002-05-15",
         };
 
         const paciente = await actualizarPaciente(codigo,data);
 
         // expect(codigo).toHaveProperty("codigo", 45);
         expect(paciente).toHaveProperty("cedula", "0999999999");
-        expect(paciente).toHaveProperty("nombres", "Pero Gonzáles");
-        expect(paciente?.fechaNacimiento).toBe("2004-12-01")
+        expect(paciente).toHaveProperty("nombres", "Carlos Ramírez");
+        expect(paciente?.fechaNacimiento).toBe("2002-05-15")
 
         /* const pacienteDB = await prisma.paciente.findUnique({
             where: { cedula: "0987654321" },
@@ -55,23 +63,23 @@ describe("Caso de uso: Actualizar Paciente", () => {
     });
 
     it("Debería lanzar un error si el código no existe", async () => {
-        await expect(eliminarPaciente(999)).rejects.toThrow("No se pudo eliminar un Paciente");
+        await expect(obtenerPaciente(1)).rejects.toThrow("No existe un Paciente con ese código");
     });
 });
 
 describe("Caso de uso: Eliminar Paciente", () => {
     it("Debería ELIMINAR un paciente correctamente", async () => {
-        const codigo = 45; // Asume que este código es válido
+        const codigo = 45
         const eliminado = await eliminarPaciente(codigo);
 
 
         
         expect(eliminado).not.toBeNull();
-        expect(eliminado).toHaveProperty("codigo", 1);
+        expect(eliminado).toHaveProperty("codigo", 45);
     });
 
     it("Debería lanzar un error si el código no existe", async () => {
-        await expect(eliminarPaciente(999)).rejects.toThrow("No se pudo eliminar un Paciente");
+        await expect(eliminarPaciente(40)).rejects.toThrow("No se pudo eliminar un Paciente");
     });
 });
 
@@ -83,13 +91,13 @@ describe("Caso de uso: Obtener un Paciente", () => {
 
         
 
-        expect(paciente).toHaveProperty("cedula", "0987654321");
-        expect(paciente).toHaveProperty("nombres", "Juan Pérez");
-        expect(paciente?.fechaNacimiento).toBe("2000-01-01");
+        expect(paciente).toHaveProperty("cedula", "0987452896");
+        expect(paciente).toHaveProperty("nombres", "Pepe Gonzáles");
+        expect(paciente?.fechaNacimiento).toBe("2004-12-12");
     });
 
     it("Debería lanzar un error si el código no existe", async () => {
-        await expect(eliminarPaciente(999)).rejects.toThrow("No se pudo eliminar un Paciente");
+        await expect(obtenerPaciente(999)).rejects.toThrow("No se pudo obtener un Paciente");
     });
 });
 
