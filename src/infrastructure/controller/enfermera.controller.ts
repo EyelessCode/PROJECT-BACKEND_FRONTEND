dd3fa8832827fed2195345c8e4971b6ed6739a24
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 19:24:20 -0500

    Update enfermera.controller.ts

--- src/infrastructure/controller/enfermera.controller.ts
+++ src/infrastructure/controller/enfermera.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { crearEnfermera } from "../../app/useCase/enfermera/crearEnfermera.useCase";
 import { eliminarEnfermera } from "../../app/useCase/enfermera/eliminarEnfermera.useCase";
 import { logger } from "../log/logger.service";
-import { actualizarEnfermeras } from "../../app/useCase/enfermera/enfermera.useCase";
+import { actualizarEnfermeras } from "../../app/useCase/enfermera.useCase";
 import { obtenerEnfermera } from "../../app/useCase/enfermera/obtenerEnfermera.useCase";
 import { obtenerEnfermeras } from "../../app/useCase/enfermera/obtenerEnfermeras.useCase";
 
