196723c35b62505625f8798e9b56976eba0d02bd
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-20 00:04:01 -0500

    Update signoPaciente.repository.ts

---
+++ src/infrastructure/repository/signoPaciente.repository.ts
@@ -0,0 +1,50 @@
+import { SignosPacientes, SignoVital } from "@prisma/client";
+import { prisma } from "../data/prisma.service";
+import { ISignoPacienteRepositorio } from "../../domain/interface/signoPaciente/signoPaciente.repository.interface";
+
+export class SignoPacienteRepositorio implements ISignoPacienteRepositorio{
+    async crearSignoPaciente(data: SignosPacientes): Promise<SignosPacientes> {
+        // throw new Error("Method not implemented.");
+        return await prisma.signosPacientes.create({
+            data:data
+        })
+    }
+
+    async obtenerSignoPaciente(codigo: number): Promise<SignosPacientes | null> {
+        // throw new Error("Method not implemented.");
+        return await prisma.signosPacientes.findUnique({
+            where:{
+                linea:codigo
+            }
+        })
+    }
+
+    async obtenerSignosPacientes(): Promise<SignosPacientes[]> {
+        // throw new Error("Method not implemented.");
+
+        return await prisma.signosPacientes.findMany()
+    }
+
+    async actualizarSignoPaciente(codigo: number, data: Partial<SignosPacientes>): Promise<SignosPacientes | null> {
+        // throw new Error("Method not implemented.");
+
+        return await prisma.signosPacientes.update({
+            where:{
+                linea:codigo
+            },
+            data:data
+        })
+    }
+
+    async eliminarSignoPaciente(codigo: number): Promise<SignosPacientes | null> {
+        // throw new Error("Method not implemented.");
+
+        return await prisma.signosPacientes.delete({
+            where:{
+                linea:codigo
+            }
+        })
+    }
+
+
+}
