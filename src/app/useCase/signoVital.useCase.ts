6f109d68d7fdd0816ccedd8d5acd41513ca25cac
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-19 20:11:47 -0500

    Update signoVital.useCase.ts

--- src/app/useCase/signoVital.useCase.ts
+++ src/app/useCase/signoVital.useCase.ts
@@ -13,6 +13,45 @@ const actualizarSignoVital=async(codigo:number,data:SignoVital):Promise<SignoVit
     }
 }
 
+const crearSignoVital=async(data:SignoVital):Promise<SignoVital>=>{
+    try {
 
+        return await repositorio.crearSignoVital(data)
+    } catch (error) {
+        console.error(`Error al crear un nuevo Signo Vital: ${error}`);
+        throw new Error('No se pudo crear un nuevo Signo Vital')
+    }
+}
+
+const eliminarSignoVital=async(codigo:number):Promise<SignoVital|null>=>{
+    try {
+
+        return await repositorio.eliminarSignoVital(codigo)
+    } catch (error) {
+        console.error(`Error al tratar de eliminar un Signo Vital`);
+        throw new Error(`No se pudo eliminar un Signo Vital`)
+    }
+}
+
+const obtenerSignosVitales=async():Promise<SignoVital[]>=>{
+    try {
+        // const obtenerSignosVitales=await prisma.signoVital.findMany()
+
+        return await repositorio.obtenerSignoVitales()
+    } catch (error) {
+        console.error(`Error al obtener todos los Signos Vitales, ${error}`);
+        throw new Error(`No se pudo obtener los Signos Vitales`)
+    }
+}
+
+const obtenerUnSignoVital=async(codigo:number):Promise<SignoVital|null>=>{
+    try {
+    
+        return await repositorio.obtenerSignoVital(codigo)
+    } catch (error) {
+        console.error(`Error al obtener un Signo Vital, ${error}`);
+        throw new Error(`No se pudo obtener un Signo Vital`)
+    }
+}
 
 export {actualizarSignoVital}
