bb8b160a478d7a6dbd93bc3f150a6dce1d1d93bc
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 18:35:47 -0500

    Update paciente.useCase.ts

--- src/app/useCase/paciente.useCase.ts
+++ src/app/useCase/paciente.useCase.ts
@@ -1,59 +1,62 @@
 import { Paciente } from "@prisma/client";
-// import { IPaciente } from "../../domain/interface/paciente/paciente.interface";
+// import { IPaciente, IPacientecasoUso } from '../../domain/interface/paciente/paciente.interface';
 // import { prisma } from "../../../infrastructure/data/prisma.service";
-import { PacienteRepositorio } from "../../infrastructure/repository/paciente.repository";
+import { PacienteRepositorio } from '../../infrastructure/repository/paciente.repository';
+import { IPacientecasoUso } from "../../domain/interface/paciente/paciente.interface";
 
-const repositorio=new PacienteRepositorio()
-
-const actualizarPaciente=async(codigo:number,data:Paciente):Promise<Paciente|null>=>{
-    try {
-
-        return await repositorio.actualizarPaciente(codigo,data)
-    } catch (error) {
-        console.error(`Error al actualizar un Paciente: ${error}`);
-        throw new Error('No se pudo actualizar un Paciente')
+export class PacienteCasoUso implements IPacientecasoUso{
+    constructor(private repositorio:PacienteRepositorio) {
+        
     }
-}
-
-const crearPaciente=async(data:Paciente):Promise<Paciente>=>{
-    try {
-
-        return await repositorio.crearPaciente(data)
-    } catch (error) {
-        console.error(`Error al crear un nuevo Paciente: ${error}`);
-        throw new Error('No se pudo crear un nuevo Paciente')
-    }
-}
-
-const eliminarPaciente=async(codigo:number):Promise<Paciente|null>=>{
-    try {
     
-        return await repositorio.eliminarPaciente(codigo)
-    } catch (error) {
-        console.error(`Error al tratar de eliminar un Paciente con el código: ${codigo}`);
-        throw new Error(`No se pudo eliminar un Paciente con el código: ${codigo}`);
+    async actualizarPaciente(codigo:number,data:Paciente):Promise<Paciente|null>{
+        try {
+    
+            return await this.repositorio.actualizarPaciente(codigo,data)
+        } catch (error) {
+            console.error(`Error al actualizar un Paciente: ${error}`);
+            throw new Error('No se pudo actualizar un Paciente')
+        }
+    }
+    
+    async crearPaciente(data:Paciente):Promise<Paciente>{
+        try {
+    
+            return await this.repositorio.crearPaciente(data)
+        } catch (error) {
+            console.error(`Error al crear un nuevo Paciente: ${error}`);
+            throw new Error('No se pudo crear un nuevo Paciente')
+        }
+    }
+    
+    async eliminarPaciente(codigo:number):Promise<Paciente|null>{
+        try {
+        
+            return await this.repositorio.eliminarPaciente(codigo)
+        } catch (error) {
+            console.error(`Error al tratar de eliminar un Paciente con el código: ${codigo}`);
+            throw new Error(`No se pudo eliminar un Paciente con el código: ${codigo}`);
+        }
+    }
+    
+    async obtenerPaciente(codigo:number):Promise<Paciente|null>{
+        try {
+    
+            return await this.repositorio.obtenerPaciente(codigo)
+        } catch (error) {
+            console.error(`Error al obtener un Paciente con el código: ${codigo}`);
+            throw new Error(`No se pudo obtener un Paciente con el código: ${codigo}`);
+        }
+    }
+    
+    async obtenerPacientes():Promise<Paciente[]>{
+        try {
+            return await this.repositorio.obtenerPacientes()
+        } catch (error) {
+            console.error(`Error al obtener todos los Pacientes, ${error}`);
+            throw new Error(`No se pudo obtener los Pacientes`)
+        }
     }
 }
 
-const obtenerPaciente=async(codigo:number):Promise<Paciente|null>=>{
-    try {
 
-        return await repositorio.obtenerPaciente(codigo)
-    } catch (error) {
-        console.error(`Error al obtener un Paciente con el código: ${codigo}`);
-        throw new Error(`No se pudo obtener un Paciente con el código: ${codigo}`);
-    }
-}
-
-const obtenerPacientes=async():Promise<Paciente[]>=>{
-    try {
-        return await repositorio.obtenerPacientes()
-    } catch (error) {
-        console.error(`Error al obtener todos los Pacientes, ${error}`);
-        throw new Error(`No se pudo obtener los Pacientes`)
-    }
-}
-
-export {actualizarPaciente,crearPaciente,eliminarPaciente,
-    obtenerPaciente,obtenerPacientes
-}
