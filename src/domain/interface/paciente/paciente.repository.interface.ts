823851cb81ff9eef5bd0e1f34bb87a94c6def0fb
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 17:27:04 -0500

    Delete paciente.repository.interface.ts

--- src/domain/interface/paciente/paciente.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Paciente } from "@prisma/client";
-
-export interface IPacienteRepositorio{
-    crearPaciente(data:Paciente):Promise<Paciente>
-    obtenerPaciente(codigo:number):Promise<Paciente|null>
-    obtenerPacientes():Promise<Paciente[]>
-    actualizarPaciente(codigo:number,data:Partial<Paciente>):Promise<Paciente|null>
-    eliminarPaciente(codigo:number):Promise<Paciente|null>
-}
