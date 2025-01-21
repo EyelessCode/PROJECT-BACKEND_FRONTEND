1847ef36e850285b068ce5fdeb7bc12f20b0bb25
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 17:27:53 -0500

    Update enfermera.interface.ts

--- src/domain/interface/enfermera/enfermera.interface.ts
+++ src/domain/interface/enfermera/enfermera.interface.ts
@@ -1,5 +1,15 @@
+import { Enfermera } from "@prisma/client"
+
 export interface IEnfermera{
     // codigo:number
     cedula:string
     nombres:string
+}
+
+export interface IEnfermeraRepositorio{
+    crearEnfermera(data:Enfermera):Promise<Enfermera>
+    obtenerEnfermera(codigo:number):Promise<Enfermera|null>
+    obtenerEnfermeras():Promise<Enfermera[]>
+    actualizarEnfermera(codigo:number,data:Partial<Enfermera>):Promise<Enfermera|null>
+    eliminarEnfermera(codigo:number):Promise<Enfermera|null>
 }
