2d63886f644b19140aa95c134bccf386868ebd8f
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 17:20:23 -0500

    Delete signoVital.repository.interface.ts

--- src/domain/interface/signoVital/signoVital.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { SignoVital } from "@prisma/client"
-
-export interface ISignoVitalRepositorio{
-    crearSignoVital(data:SignoVital):Promise<SignoVital>
-    obtenerSignoVital(codigo:number):Promise<SignoVital|null>
-    obtenerSignoVitales():Promise<SignoVital[]>
-    actualizarSignoVital(codigo:number,data:Partial<SignoVital>):Promise<SignoVital|null>
-    eliminarSignoVital(codigo:number):Promise<SignoVital|null>
-}
