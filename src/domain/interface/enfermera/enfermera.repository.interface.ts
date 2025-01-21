b1cf1f3f3f1a40268562ace37af54958fedac510
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 17:27:55 -0500

    Delete enfermera.repository.interface.ts

--- src/domain/interface/enfermera/enfermera.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Enfermera } from "@prisma/client"
-
-export interface IEnfermeraRepositorio{
-    crearEnfermera(data:Enfermera):Promise<Enfermera>
-    obtenerEnfermera(codigo:number):Promise<Enfermera|null>
-    obtenerEnfermeras():Promise<Enfermera[]>
-    actualizarEnfermera(codigo:number,data:Partial<Enfermera>):Promise<Enfermera|null>
-    eliminarEnfermera(codigo:number):Promise<Enfermera|null>
-}
