9de204e1a154b2b37925fb5fc7aba391c7cf956a
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 18:52:52 -0500

    Update signoVital.useCase.ts

--- src/app/useCase/signoVital.useCase.ts
+++ src/app/useCase/signoVital.useCase.ts
@@ -1,57 +1,62 @@
 import { SignoVital } from "@prisma/client";
 import { SignoVitalRepositorio } from "../../infrastructure/repository/signoVital.repository";
+import { ISignoVitalCasoUso } from "../../domain/interface/signoVital/signoVital.interface";
 
-const repositorio=new SignoVitalRepositorio()
-
-const actualizarSignoVital=async(codigo:number,data:SignoVital):Promise<SignoVital|null>=>{
-    try {
+export class SignoVitalCasoUso implements ISignoVitalCasoUso{
+    constructor(private repositorio:SignoVitalRepositorio) {
         
-        return await repositorio.actualizarSignoVital(codigo,data)
-    } catch (error) {
-        console.error(`Error al actualizar el signo vital ${error}`);
-        throw new Error(`No se pudo actualizar el signo vital`);
     }
-}
-
-const crearSignoVital=async(data:SignoVital):Promise<SignoVital>=>{
-    try {
-
-        return await repositorio.crearSignoVital(data)
-    } catch (error) {
-        console.error(`Error al crear un nuevo Signo Vital: ${error}`);
-        throw new Error('No se pudo crear un nuevo Signo Vital')
+    async obtenerSignoVital(codigo: number): Promise<SignoVital | null> {
+        // throw new Error("Method not implemented.");
+        try {
+        
+            return await this.repositorio.obtenerSignoVital(codigo)
+        } catch (error) {
+            console.error(`Error al obtener un Signo Vital, ${error}`);
+            throw new Error(`No se pudo obtener un Signo Vital`)
+        }
     }
-}
 
-const eliminarSignoVital=async(codigo:number):Promise<SignoVital|null>=>{
-    try {
-
-        return await repositorio.eliminarSignoVital(codigo)
-    } catch (error) {
-        console.error(`Error al tratar de eliminar un Signo Vital`);
-        throw new Error(`No se pudo eliminar un Signo Vital`)
+    async actualizarSignoVital(codigo:number,data:SignoVital):Promise<SignoVital|null>{
+        try {
+            
+            return await this.repositorio.actualizarSignoVital(codigo,data)
+        } catch (error) {
+            console.error(`Error al actualizar el signo vital ${error}`);
+            throw new Error(`No se pudo actualizar el signo vital`);
+        }
     }
-}
-
-const obtenerSignosVitales=async():Promise<SignoVital[]>=>{
-    try {
-        // const obtenerSignosVitales=await prisma.signoVital.findMany()
-
-        return await repositorio.obtenerSignoVitales()
-    } catch (error) {
-        console.error(`Error al obtener todos los Signos Vitales, ${error}`);
-        throw new Error(`No se pudo obtener los Signos Vitales`)
-    }
-}
-
-const obtenerUnSignoVital=async(codigo:number):Promise<SignoVital|null>=>{
-    try {
     
-        return await repositorio.obtenerSignoVital(codigo)
-    } catch (error) {
-        console.error(`Error al obtener un Signo Vital, ${error}`);
-        throw new Error(`No se pudo obtener un Signo Vital`)
+    async crearSignoVital(data:SignoVital):Promise<SignoVital>{
+        try {
+    
+            return await this.repositorio.crearSignoVital(data)
+        } catch (error) {
+            console.error(`Error al crear un nuevo Signo Vital: ${error}`);
+            throw new Error('No se pudo crear un nuevo Signo Vital')
+        }
+    }
+    
+    async eliminarSignoVital(codigo:number):Promise<SignoVital|null>{
+        try {
+    
+            return await this.repositorio.eliminarSignoVital(codigo)
+        } catch (error) {
+            console.error(`Error al tratar de eliminar un Signo Vital`);
+            throw new Error(`No se pudo eliminar un Signo Vital`)
+        }
+    }
+    
+    async obtenerSignosVitales():Promise<SignoVital[]>{
+        try {
+            // const obtenerSignosVitales=await prisma.signoVital.findMany()
+    
+            return await this.repositorio.obtenerSignoVitales()
+        } catch (error) {
+            console.error(`Error al obtener todos los Signos Vitales, ${error}`);
+            throw new Error(`No se pudo obtener los Signos Vitales`)
+        }
     }
 }
 
-export {actualizarSignoVital,crearSignoVital,eliminarSignoVital,obtenerSignosVitales,obtenerUnSignoVital}
+
