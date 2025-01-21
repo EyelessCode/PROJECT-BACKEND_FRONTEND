acd71314c6cd8ad895df035cc5453797af615850
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 17:20:26 -0500

    Update signoVital.interface.ts

--- src/domain/interface/signoVital/signoVital.interface.ts
+++ src/domain/interface/signoVital/signoVital.interface.ts
@@ -1,7 +1,17 @@
+import { SignoVital } from "@prisma/client"
+
 export interface ISignoVital{
     // codigo:number
     descripcion:string
     unidad:string
     valorMinimo:number
     valorMaximo:number
+}
+
+export interface ISignoVitalRepositorio{
+    crearSignoVital(data:SignoVital):Promise<SignoVital>
+    obtenerSignoVital(codigo:number):Promise<SignoVital|null>
+    obtenerSignoVitales():Promise<SignoVital[]>
+    actualizarSignoVital(codigo:number,data:Partial<SignoVital>):Promise<SignoVital|null>
+    eliminarSignoVital(codigo:number):Promise<SignoVital|null>
 }
