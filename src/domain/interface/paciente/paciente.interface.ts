e3a3a37d4fe40df51e6cd02e45439d1ea5e5a64f
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 17:27:02 -0500

    Update paciente.interface.ts

--- src/domain/interface/paciente/paciente.interface.ts
+++ src/domain/interface/paciente/paciente.interface.ts
@@ -1,6 +1,16 @@
+import { Paciente } from "@prisma/client"
+
 export interface IPaciente{
     // codigo:number
     cedula:string
     nombres:string
     fechaNacimiento:string
+}
+
+export interface IPacienteRepositorio{
+    crearPaciente(data:Paciente):Promise<Paciente>
+    obtenerPaciente(codigo:number):Promise<Paciente|null>
+    obtenerPacientes():Promise<Paciente[]>
+    actualizarPaciente(codigo:number,data:Partial<Paciente>):Promise<Paciente|null>
+    eliminarPaciente(codigo:number):Promise<Paciente|null>
 }
