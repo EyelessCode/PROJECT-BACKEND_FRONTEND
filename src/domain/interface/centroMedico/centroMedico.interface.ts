30eb23eb2ecac5de210035dd75097f8acc5b539f
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 17:28:54 -0500

    Update centroMedico.interface.ts

--- src/domain/interface/centroMedico/centroMedico.interface.ts
+++ src/domain/interface/centroMedico/centroMedico.interface.ts
@@ -1,5 +1,15 @@
+import { CentroMedico } from "@prisma/client"
+
 export interface ICentroMedico{
     // codigo:number
     nombre:string
     direccion:string
+}
+
+export interface ICentroMedicoRepositorio{
+    crearCentroMedico(data:CentroMedico):Promise<CentroMedico>
+    obtenerCentroMedico(codigo:number):Promise<CentroMedico|null>
+    obtenerCentrosMedicos():Promise<CentroMedico[]>
+    actualizarCentroMedico(codigo:number,data:Partial<CentroMedico>):Promise<CentroMedico|null>
+    eliminarCentroMedico(codigo:number):Promise<CentroMedico|null>
 }
