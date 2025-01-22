e94d84d6a724557154c2c175b9f856507203c809
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-22 16:58:31 -0500

    Update enfermera.ts

--- src/routes/enfermera.ts
+++ src/routes/enfermera.ts
@@ -1,10 +1,14 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express"
 import { EnfermeraController } from "../infrastructure/controller/enfermera.controller"
 import { validarCodigo, validarEnfermera } from "../infrastructure/validation/validation.middleware"
+import path from "path"
 
 const ruta=Router()
 const controlador=new EnfermeraController()
 
+ruta.get("/html",(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname,"..","..","view","enfermera.html"))
+})
 ruta.get("/",controlador.controladorObtenerEnfermeras.bind(controlador))
 ruta.get("/:codigo",validarCodigo,controlador.controladorObtenerEnfermera.bind(controlador))
 ruta.post("/",validarEnfermera,controlador.controladorCrearEnfermera.bind(controlador))
