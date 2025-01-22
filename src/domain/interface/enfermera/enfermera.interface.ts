bfa3c7bf00a3a13318e74753470301c91d69642e
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 19:38:12 -0500

    Update enfermera.interface.ts

--- src/domain/interface/enfermera/enfermera.interface.ts
+++ src/domain/interface/enfermera/enfermera.interface.ts
@@ -13,3 +13,11 @@ export interface IEnfermeraRepositorio{
     actualizarEnfermera(codigo:number,data:Partial<Enfermera>):Promise<Enfermera|null>
     eliminarEnfermera(codigo:number):Promise<Enfermera|null>
 }
+
+export interface IEnfermeraCasoUso{
+    crearEnfermera(data:Enfermera):Promise<Enfermera>
+    obtenerEnfermera(codigo:number):Promise<Enfermera|null>
+    obtenerEnfermeras():Promise<Enfermera[]>
+    actualizarEnfermera(codigo:number,data:Partial<Enfermera>):Promise<Enfermera|null>
+    eliminarEnfermera(codigo:number):Promise<Enfermera|null>
+}
