532d47582e84f83ed7e55e40798996f90541a1b4
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-21 19:38:18 -0500

    Update enfermera.useCase.ts

--- src/app/useCase/enfermera.useCase.ts
+++ src/app/useCase/enfermera.useCase.ts
@@ -1,57 +1,59 @@
 import { Enfermera } from "@prisma/client";
 // import { IEnfermera } from "../../../domain/interface/enfermera/enfermera.interface";
 import { EnfermeraRepositorio } from "../../infrastructure/repository/enfermera.repository";
+import { IEnfermeraCasoUso } from "../../domain/interface/enfermera/enfermera.interface";
 // import { prisma } from "../../../infrastructure/data/prisma.service";
 
-const repositorio=new EnfermeraRepositorio()
+export class EnfermeraCasoUso implements IEnfermeraCasoUso{
+    constructor(private repositorio:EnfermeraRepositorio){}
 
-const actualizarEnfermeras=async(codigo:number,data:Enfermera):Promise<Enfermera|null>=>{
-    try {
-
-        return await repositorio.actualizarEnfermera(codigo,data)
-    } catch (error) {
-        console.error(`Error al actualizar una Enfermera: ${error}`);
-        throw new Error('No se pudo actualizar una Enfermera')
+    async actualizarEnfermera(codigo:number,data:Partial<Enfermera>):Promise<Enfermera|null>{
+        try {
+    
+            return await this.repositorio.actualizarEnfermera(codigo,data)
+        } catch (error) {
+            console.error(`Error al actualizar una Enfermera: ${error}`);
+            throw new Error('No se pudo actualizar una Enfermera')
+        }
+    }
+    
+    async crearEnfermera(data:Enfermera):Promise<Enfermera>{
+        try {
+    
+            return await this.repositorio.crearEnfermera(data)
+        } catch (error) {
+            console.error(`Error al crear una Enfermera: ${error}`);
+            throw new Error('No se pudo crear una nuevo Enfermera')
+        }
+    }
+    
+    async eliminarEnfermera(codigo:number):Promise<Enfermera|null>{
+        try {
+    
+            return await this.repositorio.eliminarEnfermera(codigo)
+        } catch (error) {
+            console.error(`Error al eliminar una Enfermera: ${error}`);
+            throw new Error('No se pudo eliminar una Enfermera')
+        }
+    }
+    
+    async obtenerEnfermera(codigo:number):Promise<Enfermera|null>{
+        try {
+    
+            return await this.repositorio.obtenerEnfermera(codigo)
+        } catch (error) {
+            console.error(`Error al obtener una Enfermera: ${error}`);
+            throw new Error('No se pudo obtener una Enfermera')
+        }
+    }
+    
+    async obtenerEnfermeras():Promise<Enfermera[]>{
+        try {
+            return await this.repositorio.obtenerEnfermeras()
+        } catch (error) {
+            console.error(`Error al obtener las Enfermeras: ${error}`);
+            throw new Error('No se pudo obtener las Enfermeras')
+        }
     }
 }
 
-const crearEnfermera=async(data:Enfermera):Promise<Enfermera>=>{
-    try {
-
-        return await repositorio.crearEnfermera(data)
-    } catch (error) {
-        console.error(`Error al crear una Enfermera: ${error}`);
-        throw new Error('No se pudo crear una nuevo Enfermera')
-    }
-}
-
-const eliminarEnfermera=async(codigo:number):Promise<Enfermera|null>=>{
-    try {
-
-        return await repositorio.eliminarEnfermera(codigo)
-    } catch (error) {
-        console.error(`Error al eliminar una Enfermera: ${error}`);
-        throw new Error('No se pudo eliminar una Enfermera')
-    }
-}
-
-const obtenerEnfermera=async(codigo:number):Promise<Enfermera|null>=>{
-    try {
-
-        return await repositorio.obtenerEnfermera(codigo)
-    } catch (error) {
-        console.error(`Error al obtener una Enfermera: ${error}`);
-        throw new Error('No se pudo obtener una Enfermera')
-    }
-}
-
-const obtenerEnfermeras=async():Promise<Enfermera[]>=>{
-    try {
-        return await repositorio.obtenerEnfermeras()
-    } catch (error) {
-        console.error(`Error al obtener las Enfermeras: ${error}`);
-        throw new Error('No se pudo obtener las Enfermeras')
-    }
-}
-
-export {actualizarEnfermeras,crearEnfermera,eliminarEnfermera,obtenerEnfermera,obtenerEnfermeras}
