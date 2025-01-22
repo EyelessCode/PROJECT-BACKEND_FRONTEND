88f421ae88ecf6e348d41a7debef7198123726fb
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-22 17:24:30 -0500

    Update centroMedico.ts

--- src/routes/centroMedico.ts
+++ src/routes/centroMedico.ts
@@ -1,10 +1,14 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { CentroMedicoController } from "../infrastructure/controller/centroMedico.controller";
 import { validarCentroMedico, validarCodigo } from "../infrastructure/validation/validation.middleware";
+import path from "path";
 
 const ruta=Router()
 const controlador=new CentroMedicoController()
 
+ruta.get("/html",(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname,"..","..","view","centroMedico.html"))
+})
 ruta.get("/",controlador.controladorObtenerCentrosMedicos.bind(controlador))
 ruta.get("/:codigo",validarCodigo,controlador.controladorObtenerCentroMedico.bind(controlador))
 ruta.post("/",validarCentroMedico,controlador.controladorCrearCentroMedico.bind(controlador))
