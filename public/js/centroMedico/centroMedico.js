d7b9ee6a02bc5130a1c1cebfcc0c7d70f71ddaf3
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-22 20:12:47 -0500

    Update centroMedico.js

---
+++ public/js/centroMedico/centroMedico.js
@@ -0,0 +1,35 @@
+
+const API_URL="http://localhost:4000/comsulmed/centroMedico"
+
+const btnBuscarPaciente=document.getElementById("btnBuscarPaciente")
+const tablaCentroMedico=document.getElementById("tablaCentroMedico")
+
+tablaCentroMedico.addEventListener()
+
+
+async function fetchCentroMedico() {
+    try {
+        const respuesta=await fetch(API_URL)
+        if (!respuesta) {
+            throw new Error(`No se pudo obtener la información del centro médico`)
+        }
+
+        const centroMedico=await respuesta.json()
+        renderizarCentroMedico(centroMedico)
+    } catch (error) {
+        
+    }
+}
+
+function renderizarCentroMedico(centroMedico) {
+    tablaCentroMedico.innerHTML=""
+
+    centroMedico.forEach(centroMedicoEach => {
+        const fila=document.createElement("tr")
+        fila.innerHTML=`
+            <td>${centroMedicoEach.nombre}</td>
+            <td>${centroMedicoEach.direccion}</td>`
+
+            tablaCentroMedico.appendChild(fila)
+    });
+}
