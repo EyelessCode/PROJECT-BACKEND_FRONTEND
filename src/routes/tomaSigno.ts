02f8b862f2c78bb00cff75be397c6b2edc062068
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-22 23:38:28 -0500

    Update tomaSigno.ts

--- src/routes/tomaSigno.ts
+++ src/routes/tomaSigno.ts
@@ -1,10 +1,14 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express"
 import { TomaSignoController } from "../infrastructure/controller/tomaSigno.controller"
 import { validarCodigo } from "../infrastructure/validation/validation.middleware"
+import path from "path"
 
 const ruta=Router()
 const controlador=new TomaSignoController()
 
+ruta.get("/html",(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname,"..","..","view","tomaSigno.html"))
+})
 ruta.post("/",controlador.registrarTomaSigno.bind(controlador))
 
 export{ruta}
