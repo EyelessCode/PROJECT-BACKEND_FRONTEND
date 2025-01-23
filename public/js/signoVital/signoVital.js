373c7c63c3c7a0d003c39d181c7fd13998ff5979
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-22 22:48:38 -0500

    Update signoVital.js

---
+++ public/js/signoVital/signoVital.js
@@ -0,0 +1,81 @@
+
+const API_URL="http://localhost:4000/comsulmed/signoVital"
+
+const formularioSignoVital=document.getElementById("formularioSignoVital")
+const txtDescripcionVital=document.getElementById("txtDescripcionVital")
+const txtUnidadVital=document.getElementById("txtUnidadVital")
+const txtValorMin=document.getElementById("txtValorMin")
+const txtValorMax=document.getElementById("txtValorMax")
+const tablaSignosVitales=document.getElementById("tablaSignosVitales")
+
+document.addEventListener("DOMContentLoaded",fetchSignoVital)
+
+formularioSignoVital.addEventListener("submit",async(event)=>{
+    event.preventDefault()
+
+    const formulario=new FormData(formularioSignoVital)
+    const data={
+        descripcion:formulario.get(descripcion),
+        unidad:formulario.get(unidad),
+        valorMinimo:parseFloat(formulario.get(valorMinimo)),
+        valorMaximo:parseFloat(formulario.get(valorMaximo))
+    }
+
+    try {
+        // const respuesta=await fetch(API_URL)
+
+        const respuesta=await fetch(API_URL,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(data)
+        })
+
+        if (!respuesta.ok) {
+            throw new Error(`Error ${respuesta.status} ${respuesta.statusText}`)
+        }
+
+        const signoVital=await respuesta.json()
+        filaSignoVital(signoVital)
+    } catch (error) {
+        console.error(error)
+        alert("Hubo un problema al registrar el signo vital")
+    }
+})
+
+async function fetchSignoVital() {
+    try {
+        const respuesta=await fetch(API_URL)
+        if (!respuesta.ok) {
+            throw new Error(`Error al obtener los signos vitales ${respuesta.status} ${respuesta.statusText}`)
+        }
+
+        const signoVital=await respuesta.json()
+        renderizarSignoVital(signoVital)
+        render
+    } catch (error) {
+        console.error(error)
+        alert("Hubo un problema al cargar los signos vitales")
+    }
+}
+
+function renderizarSignoVital(signoVital) {
+    tablaSignosVitales.innerHTML=""
+
+    signoVital.forEach(signoVitalEach => {
+        generadorFila(signoVitalEach)
+    });
+}
+
+function generadorFila(signo) {
+    const fila=document.createElement("tr")
+
+    fila.innerHTML=`
+        <td>${signo.descripcion}</td>
+        <td>${signo.unidad}</td>
+        <td>${signo.valorMinimo}</td>
+        <td>${signo.valorMaximo}</td>`
+
+    tablaSignosVitales.appendChild(fila)
+}
