300f1e07503c3195c9d8adcb48f7b7658124249d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-23 00:26:43 -0500

    Update tomaSigno.controller.ts

--- src/infrastructure/controller/tomaSigno.controller.ts
+++ src/infrastructure/controller/tomaSigno.controller.ts
@@ -13,11 +13,21 @@ const casoUso=new TomaSignoCasoUso(
 )
 
 export class TomaSignoController{
-    async registrarTomaSigno(req:Request,res:Response):Promise<any>{
+    async controladorRegistrarTomaSigno(req:Request,res:Response):Promise<any>{
         try {
             const data=req.body
             const tomaSigno=await casoUso.registrarTomaSigno(data)
             return res.status(201).json(tomaSigno)
+        } catch (error) {
+            console.error(error)
+            return res.status(400).json({ message: error })
+        }
+    }
+
+    async controladorObtenerTomaSignos(req:Request,res:Response):Promise<any>{
+        try {
+            const tomaSignos=await casoUso.obtenerTomaSignos()
+            return res.status(200).json(tomaSignos)
         } catch (error) {
             console.error(error)
             return res.status(400).json({ message: error })
