6ed36ed5a464402a93e6a8824252d76d90750894
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-26 16:56:34 -0500

    Update signoPaciente.ts

--- src/routes/signoPaciente.ts
+++ src/routes/signoPaciente.ts
@@ -1,10 +1,14 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { SignoPacienteController } from "../infrastructure/controller/signoPaciente.controller";
+import path from "path";
 // import { validarCodigo } from "../infrastructure/validation/validation.middleware";
 
 const ruta=Router()
 const controlador=new SignoPacienteController()
 
+ruta.get("/html",(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname,"..","..","view","signoPaciente.html"))
+})
 ruta.get("/",controlador.controladorObtenerSignosPacientes.bind(controlador))
 ruta.post("/",controlador.controladorRegistrarSignoPaciente.bind(controlador))
 
