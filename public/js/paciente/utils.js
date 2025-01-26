6b9ea9b58f0961ba7f97c603d70f2a05c4a6caba
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-25 20:48:12 -0500

    Update utils.js

---
+++ public/js/paciente/utils.js
@@ -0,0 +1,13 @@
+export function calcularEdad(fechaNacimiento) {
+    const hoy = new Date();
+    let edadCalculada = hoy.getFullYear() - fechaNacimiento.getFullYear();
+    const mes = hoy.getMonth() - fechaNacimiento.getMonth();
+    const dia = hoy.getDate() - fechaNacimiento.getDate();
+
+    // Ajuste para el caso en que el cumpleaños no ha sido cumplido este año aún
+    if (mes < 0 || (mes === 0 && dia < 0)) {
+        edadCalculada--;
+    }
+
+    return edadCalculada;
+}
