1e8c5ba20e88ffa5d3e5fb856ac3923649855774
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-26 10:45:13 -0500

    Update utils.js

---
+++ public/js/enfermera/utils.js
@@ -0,0 +1,30 @@
+export function mostrarEnfermerasTabla(enfermeras) {
+    const cuerpoTabla = document.getElementById('cuerpoTablaEnfermeras');
+    cuerpoTabla.innerHTML = '';
+
+    enfermeras.forEach(enfermera => {
+        const fila = document.createElement('tr');
+
+        let celda = document.createElement('td');
+        celda.textContent = enfermera.codigo;
+        fila.appendChild(celda);
+
+        celda = document.createElement('td');
+        celda.textContent = enfermera.cedula;
+        fila.appendChild(celda);
+
+        celda = document.createElement('td');
+        celda.textContent = enfermera.nombres;
+        fila.appendChild(celda);
+
+        celda = document.createElement('td');
+        celda.textContent = enfermera.apellidos;
+        fila.appendChild(celda);
+
+        celda = document.createElement('td');
+        celda.textContent = enfermera.especialidad;
+        fila.appendChild(celda);
+
+        cuerpoTabla.appendChild(fila);
+    });
+}
