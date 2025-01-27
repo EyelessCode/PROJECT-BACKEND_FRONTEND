30046e3257288b47cdd6ecdc6a90e035d1a0504d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-26 20:02:26 -0500

    Update centroMedico.js

--- public/js/centroMedico/centroMedico.js
+++ public/js/centroMedico/centroMedico.js
@@ -26,31 +26,4 @@ document.getElementById('btnBuscarCentro').addEventListener('click', async () =>
     }
 });
 
-async function cargarCentrosMedicos() {
-    try {
-        console.log('Cargando Centros Médicos');
-        const response = await fetch(API_URL);
-        const centros = await response.json();
-        console.log('Centros Médicos Cargados:', centros);
-        return centros;
-    } catch (error) {
-        console.error('Error:', error);
-        alert('Error al cargar los centros médicos');
-    }
-}
 
-async function buscarCentroMedico(codigo) {
-    try {
-        console.log('Buscando Centro Médico con Código:', codigo);
-        const response = await fetch(`${API_URL}/${codigo}`);
-        if (response.ok) {
-            const centro = await response.json();
-            return { success: true, centro: centro };
-        } else {
-            return { success: false, message: 'Centro Médico no encontrado' };
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        return { success: false, message: 'Error al buscar el centro médico' };
-    }
-}
