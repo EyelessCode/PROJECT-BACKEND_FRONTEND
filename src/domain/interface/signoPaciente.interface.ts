4aeb6af097d961cc26fc0d021ac6ec8fcb705618
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-28 20:40:27 -0500

    Update signoPaciente.interface.ts

--- src/domain/interface/signoPaciente.interface.ts
+++ src/domain/interface/signoPaciente.interface.ts
@@ -1,12 +1,12 @@
 import { SignosPacientes } from "@prisma/client"
 
 export interface ISignoPacienteRepositorio{
-    crearSignoPaciente(data:SignosPacientes):Promise<SignosPacientes>
+    crearSignoPaciente(data:SignosPacientes[]):Promise<SignosPacientes[]>
     obtenerSignosPacientes():Promise<SignosPacientes[]>
 }
 
 export interface ISignoPacienteCasoUso{
-    registrarSignoPaciente(data:SignosPacientes):Promise<SignosPacientes>
+    registrarSignoPaciente(data:SignosPacientes[]):Promise<SignosPacientes[]>
     obtenerSignosPacientes():Promise<SignosPacientes[]>
 
 }
