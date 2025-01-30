ee2865156a3194db0780655862817f0cbf1c1de0
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-30 03:25:50 -0500

    Limpieza

--- dist/util/error.until.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttp = void 0;
-const handleHttp = (res, error, errorBruto) => {
-    console.error(errorBruto);
-    res.status(500).json({ error });
-};
-exports.handleHttp = handleHttp;
-//# sourceMappingURL=error.until.js.map
