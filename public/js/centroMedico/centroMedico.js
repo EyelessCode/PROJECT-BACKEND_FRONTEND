ef4be0863d57f1383d290d662deb5ca84ae1289a
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-02-08 12:35:39 -0500

    Update centroMedico.js

--- public/js/centroMedico/centroMedico.js
+++ public/js/centroMedico/centroMedico.js
@@ -1,29 +1,27 @@
-import {mostrarCentrosEnTabla} from './utils.js';
-import {buscarCentroMedico,cargarCentrosMedicos} from './load.js';
+import { mostrarCentrosEnTabla } from './utils.js';
+import { buscarCentroMedico, cargarCentrosMedicos } from './load.js';
 
 
 document.getElementById('btnListarCentros').addEventListener('click', async () => {
-    console.log('Listar Centros Médicos');
-    document.getElementById('tablaCentros').style.display = 'block';
-    document.getElementById('btnOcultarCentros').style.display = 'inline';
-    const centros = await cargarCentrosMedicos();
-    mostrarCentrosEnTabla(centros);  // Mostrar los centros médicos en la tabla
-});
+    console.log('Listar Centros Médicos')
+    document.getElementById('tablaCentros').style.display = 'block'
+    document.getElementById('btnOcultarCentros').style.display = 'inline'
+    const centros = await cargarCentrosMedicos()
+    mostrarCentrosEnTabla(centros)
+})
 
 document.getElementById('btnOcultarCentros').addEventListener('click', () => {
-    console.log('Ocultar Tabla');
-    document.getElementById('tablaCentros').style.display = 'none';
-    document.getElementById('btnOcultarCentros').style.display = 'none';
-});
+    console.log('Ocultar Tabla')
+    document.getElementById('tablaCentros').style.display = 'none'
+    document.getElementById('btnOcultarCentros').style.display = 'none'
+})
 
 document.getElementById('btnBuscarCentro').addEventListener('click', async () => {
-    const codigo = document.getElementById('buscarCentro').value;
-    const result = await buscarCentroMedico(codigo);
+    const codigo = document.getElementById('buscarCentro').value
+    const result = await buscarCentroMedico(codigo)
     if (result.success) {
-        alert(`Centro Médico encontrado: ${result.centro.nombre} en ${result.centro.direccion}`);
+        alert(`Centro Médico encontrado: ${result.centro.nombre} en ${result.centro.direccion}`)
     } else {
-        alert(result.message);
+        alert(result.message)
     }
-});
-
-
+})
