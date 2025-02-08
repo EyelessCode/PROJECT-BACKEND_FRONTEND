7e0af5a92f864c34a89b0e58f650c4f9c0d4c42e
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-02-08 11:56:04 -0500

    Update paciente.js

--- public/js/paciente/paciente.js
+++ public/js/paciente/paciente.js
@@ -1,40 +1,40 @@
-import {calcularEdad,mostrarTablaPaciente,} from './utils.js';
-import {API_URL,cargarPacientes} from './load.js';
+import { calcularEdad, mostrarTablaPaciente, } from './utils.js';
+import { API_URL, cargarPacientes } from './load.js';
 
 
 document.getElementById('btnRegistrar').addEventListener('click', () => {
-    console.log('Registrar Paciente');
-    document.getElementById('formRegistro').style.display = 'block';
-    document.getElementById('tablaPacientes').style.display = 'none';
-    document.getElementById('btnDesaparecer').style.display = 'none';
-});
+    console.log('Registrar Paciente')
+    document.getElementById('formRegistro').style.display = 'block'
+    document.getElementById('tablaPacientes').style.display = 'none'
+    document.getElementById('btnDesaparecer').style.display = 'none'
+})
 
-document.getElementById('btnListar').addEventListener('click',async () => {
-    console.log('Listar Pacientes');
-    document.getElementById('tablaPacientes').style.display = 'block';
-    document.getElementById('formRegistro').style.display = 'none';
-    document.getElementById('btnDesaparecer').style.display = 'inline';
-    const paciente=await cargarPacientes()
+document.getElementById('btnListar').addEventListener('click', async () => {
+    console.log('Listar Pacientes')
+    document.getElementById('tablaPacientes').style.display = 'block'
+    document.getElementById('formRegistro').style.display = 'none'
+    document.getElementById('btnDesaparecer').style.display = 'inline'
+    const paciente = await cargarPacientes()
     mostrarTablaPaciente(paciente)
-});
+})
 
 document.getElementById('btnDesaparecer').addEventListener('click', () => {
-    console.log('Ocultar Tabla');
-    document.getElementById('tablaPacientes').style.display = 'none';
-    document.getElementById('btnDesaparecer').style.display = 'none';
-});
+    console.log('Ocultando Tabla...')
+    document.getElementById('tablaPacientes').style.display = 'none'
+    document.getElementById('btnDesaparecer').style.display = 'none'
+})
 
 document.getElementById('btnCancelar').addEventListener('click', () => {
-    console.log('Cancelar Registro');
-    document.getElementById('formPaciente').reset();
-    document.getElementById('formRegistro').style.display = 'none';
-});
+    console.log('Cancelando Registro')
+    document.getElementById('formPaciente').reset()
+    document.getElementById('formRegistro').style.display = 'none'
+})
 
 document.getElementById('formPaciente').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    console.log('Submit Paciente');
-    const fechaNacimiento = new Date(document.getElementById('fechaNacimiento').value);
-    const edad = calcularEdad(fechaNacimiento);
+    e.preventDefault() //! Evita que se recargue la página
+    console.log('Entrega de Paciente')
+    const fechaNacimiento = new Date(document.getElementById('fechaNacimiento').value)
+    const edad = calcularEdad(fechaNacimiento)
 
     const paciente = {
         cedula: document.getElementById('cedula').value,
@@ -48,9 +48,9 @@ document.getElementById('formPaciente').addEventListener('submit', async (e) => 
         direccion: document.getElementById('direccion').value,
         correo: document.getElementById('correo').value,
         ocupacion: document.getElementById('ocupacion').value
-    };
+    }
 
-    console.log('Datos del Paciente:', paciente);
+    console.log('Datos del Paciente:', paciente)
 
     try {
         const response = await fetch(API_URL, {
@@ -59,41 +59,39 @@ document.getElementById('formPaciente').addEventListener('submit', async (e) => 
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(paciente)
-        });
+        })
 
-        const responseData = await response.json();
-        console.log('Respuesta del Servidor:', responseData);
+        const responseData = await response.json()
+        console.log('Respuesta del Servidor:', responseData)
 
         if (!response.ok) {
-            alert('Error al registrar el paciente');
+            alert('Error al registrar el paciente')
 
         }
-        alert('Paciente registrado correctamente');
-        document.getElementById('formPaciente').reset();
-        document.getElementById('formRegistro').style.display = 'none';
-        cargarPacientes();
+        alert('Paciente registrado correctamente')
+        document.getElementById('formPaciente').reset()
+        document.getElementById('formRegistro').style.display = 'none'
+        cargarPacientes()
     } catch (error) {
-        console.error('Error:', error);
-        console.error('Error en la respuesta del servidor:', responseData);
-        alert(`Error al registrar el paciente: ${responseData.message || 'Desconocido'}`);
+        console.error('Error:', error)
+        console.error('Error en la respuesta del servidor:', responseData)
+        alert(`Error al registrar el paciente: ${responseData.message || 'Desconocido'}`)
     }
-});
-
-
+})
 
 document.getElementById('btnBuscar').addEventListener('click', async () => {
-    const codigo = document.getElementById('buscarCodigo').value;
+    const codigo = document.getElementById('buscarCodigo').value
     try {
-        console.log('Buscando Paciente con Código:', codigo);
-        const response = await fetch(`${API_URL}/${codigo}`);
-        if (response.ok) {
-            const paciente = await response.json();
-            alert(`Paciente encontrado: ${paciente.nombres} ${paciente.apellidos}`);
-        } else {
-            alert('Paciente no encontrado');
+        console.log('Buscando Paciente con Código:', codigo)
+        const response = await fetch(`${API_URL}/${codigo}`)
+        if (!response.ok) {
+            alert('Paciente no encontrado')
         }
+
+        const paciente = await response.json()
+        alert(`Paciente encontrado: ${paciente.nombres} ${paciente.apellidos}`)
     } catch (error) {
-        console.error('Error:', error);
-        alert('Error al buscar el paciente');
+        console.error('Error:', error)
+        alert('Error al buscar el paciente')
     }
-});
+})
