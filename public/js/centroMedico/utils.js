2444ecdc148c669b559856e56947f7ec1a5abcaa
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-02-08 17:52:59 -0500

    Update utils.js

--- public/js/centroMedico/utils.js
+++ public/js/centroMedico/utils.js
@@ -1,31 +1,30 @@
 export function mostrarCentrosEnTabla(centros) {
-    const cuerpoTabla = document.getElementById('cuerpoTablaCentros');
-    cuerpoTabla.innerHTML = '';  // Limpiar la tabla antes de agregar datos
+    const cuerpoTabla = document.getElementById('cuerpoTablaCentros')
+    cuerpoTabla.innerHTML = ''
 
     centros.forEach(centro => {
-        const fila = document.createElement('tr');
+        const fila = document.createElement('tr')
 
-        // Crear celdas y agregar los datos en el orden correcto
-        let celda = document.createElement('td');
-        celda.textContent = centro.codigo;
-        fila.appendChild(celda);
+        let celda = document.createElement('td')
+        celda.textContent = centro.codigo
+        fila.appendChild(celda)
 
-        celda = document.createElement('td');
-        celda.textContent = centro.RUC;
-        fila.appendChild(celda);
+        celda = document.createElement('td')
+        celda.textContent = centro.RUC
+        fila.appendChild(celda)
 
-        celda = document.createElement('td');
-        celda.textContent = centro.nombre;
-        fila.appendChild(celda);
+        celda = document.createElement('td')
+        celda.textContent = centro.nombre
+        fila.appendChild(celda)
 
-        celda = document.createElement('td');
-        celda.textContent = centro.direccion;
-        fila.appendChild(celda);
+        celda = document.createElement('td')
+        celda.textContent = centro.direccion
+        fila.appendChild(celda)
 
-        celda = document.createElement('td');
-        celda.textContent = centro.telefono;
-        fila.appendChild(celda);
+        celda = document.createElement('td')
+        celda.textContent = centro.telefono
+        fila.appendChild(celda)
 
-        cuerpoTabla.appendChild(fila);
-    });
+        cuerpoTabla.appendChild(fila)
+    })
 }
