b68ee5d29beede354bac57aa30a4be61f56f7ecd
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-02-08 17:56:01 -0500

    Update enfermera.js

--- public/js/enfermera/enfermera.js
+++ public/js/enfermera/enfermera.js
@@ -1,29 +1,27 @@
-import {mostrarEnfermerasTabla} from './utils.js';
-import {buscarEnfermera,cargarEnfermeras} from './load.js';
+import { mostrarEnfermerasTabla } from './utils.js';
+import { buscarEnfermera, cargarEnfermeras } from './load.js';
 
 
 document.getElementById('btnListarEnfermeras').addEventListener('click', async () => {
-    console.log('Listar Enfermeras');
-    document.getElementById('tablaEnfermeras').style.display = 'block';
-    document.getElementById('btnOcultarEnfermeras').style.display = 'inline';
-    const enfermera = await cargarEnfermeras();
-    mostrarEnfermerasTabla(enfermera);
-});
+    console.log('Listar Enfermeras')
+    document.getElementById('tablaEnfermeras').style.display = 'block'
+    document.getElementById('btnOcultarEnfermeras').style.display = 'inline'
+    const enfermera = await cargarEnfermeras()
+    mostrarEnfermerasTabla(enfermera)
+})
 
 document.getElementById('btnOcultarEnfermeras').addEventListener('click', () => {
-    console.log('Ocultar Tabla');
-    document.getElementById('tablaEnfermeras').style.display = 'none';
-    document.getElementById('btnOcultarEnfermeras').style.display = 'none';
-});
+    console.log('Ocultar Tabla')
+    document.getElementById('tablaEnfermeras').style.display = 'none'
+    document.getElementById('btnOcultarEnfermeras').style.display = 'none'
+})
 
 document.getElementById('btnBuscarEnfermera').addEventListener('click', async () => {
-    const codigo = document.getElementById('buscarEnfermera').value;
-    const result = await buscarEnfermera(codigo);
+    const codigo = document.getElementById('buscarEnfermera').value
+    const result = await buscarEnfermera(codigo)
     if (result.success) {
-        alert(`Enfermera encontrada: ${result.enfermera.nombres} en ${result.enfermera.especialidad}`);
+        alert(`Enfermera encontrada: ${result.enfermera.nombres} en ${result.enfermera.especialidad}`)
     } else {
-        alert(result.message);
+        alert(result.message)
     }
-});
-
-
+})
