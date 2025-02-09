3f6cee164ebaaf405fbbde2ae7b92d5d9a91af7c
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-02-08 23:31:05 -0500

    Update load.js

--- public/js/enfermera/load.js
+++ public/js/enfermera/load.js
@@ -1,31 +1,30 @@
-const API_URL = "http://localhost:4000/comsulmed/enfermera";
-
+const API_URL = "http://localhost:4000/comsulmed/enfermera"
 
 export async function cargarEnfermeras() {
     try {
-        console.log('Cargando Enfermeras');
-        const response = await fetch(API_URL);
-        const enfermeras = await response.json();
-        console.log('Enfermeras Cargadas:', enfermeras);
-        return enfermeras;
+        console.log('Cargando Enfermeras')
+        const response = await fetch(API_URL)
+        const enfermeras = await response.json()
+        console.log('Enfermeras Cargadas:', enfermeras)
+        return enfermeras
     } catch (error) {
-        console.error('Error:', error);
-        alert('Error al cargar las Enfermeras');
+        console.error('Error:', error)
+        alert('Error al cargar las Enfermeras')
     }
 }
 
 export async function buscarEnfermera(codigo) {
     try {
-        console.log('Buscando Enfermera con Código:', codigo);
-        const response = await fetch(`${API_URL}/${codigo}`);
+        console.log('Buscando Enfermera con Código:', codigo)
+        const response = await fetch(`${API_URL}/${codigo}`)
         if (response.ok) {
-            const enfermera = await response.json();
-            return { success: true, enfermera: enfermera };
+            const enfermera = await response.json()
+            return { success: true, enfermera: enfermera }
         } else {
-            return { success: false, message: 'Enfermera no encontrada' };
+            return { success: false, message: 'Enfermera no encontrada' }
         }
     } catch (error) {
-        console.error('Error:', error);
-        return { success: false, message: 'Error al buscar la Enfermera' };
+        console.error('Error:', error)
+        return { success: false, message: 'Error al buscar la Enfermera' }
     }
 }
