169e030af4c1057b912e8951d0553e1d2fdf1e24
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-02-12 20:38:09 -0500

    Sombrita en las letras del logo

--- public/js/paciente/utils.js
+++ public/js/paciente/utils.js
@@ -1,4 +1,4 @@
-import {editarPaciente,eliminarPaciente,actualizarPaciente,cargarPacientes} from './load.js';
+import { editarPaciente, eliminarPaciente, actualizarPaciente, cargarPacientes } from './load.js';
 
 export function calcularEdad(fechaNacimiento) {
     const hoy = new Date() //! Obtiene la fecha actual
@@ -137,16 +137,30 @@ export function generarFormularioEdicion(paciente) {
                 <input type="text" id="ocupacion" value="${paciente.ocupacion || 'ninguno'}" required>
                 
                 <div class="form-buttons">
-                    <button id="btnActualizar" class="btn-accion" type="submit">Actualizar</button>
-                    <button id="btnCancelar" class="btn-accion" type="button">Cancelar</button>
+                    <button id="btnActualizar" class="btn-accion" type="submit">
+                    Actualizar
+                    <svg class="svgEditar xmlns="http://www.w3.org/2000/svg"
+                                height="24px" viewBox="0 -960 960 960" width="24px"
+                                fill="#000000"><path
+                                    d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" /></svg>
+                    </button>
+
+                    <button id="btnCancelar" class="btn-accion" type="button">
+                    Cancelar
+                    <svg class="svgCancelar"
+                                xmlns="http://www.w3.org/2000/svg" height="24px"
+                                viewBox="0 -960 960 960" width="24px"
+                                fill="#000000"><path
+                                    d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                    </button>
                 </div>
             </form>
         </div>
     `;
-    
+
     document.getElementById('tablaPacientes').style.display = 'none';
     document.getElementById('btnDesaparecer').style.display = 'none';
-    
+
     document.getElementById('formPaciente').addEventListener('submit', (e) => actualizarPaciente(e, paciente.codigo));
     document.getElementById('btnCancelar').addEventListener('click', () => volverAInicio());
 }
